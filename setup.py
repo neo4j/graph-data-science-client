3b5b151d04e8be53486b7d0f8c50be9c2a68b35e
Adam Schill Collberg <adam.schill.collberg@protonmail.com>
2021-12-22 09:28:28 +0100

    Format setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setuptools.setup(
     description="Python bindings for the Neo4j Graph Data Science library",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    license='Apache License 2.0',
+    license="Apache License 2.0",
     url="https://github.com/neo4j/graph-data-science",
     classifiers=[
         "Programming Language :: Python :: 3",
