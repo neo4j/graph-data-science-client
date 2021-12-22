a3eb4f2af9c89217a47161a74da16f1c4ce0a3d9
Adam Schill Collberg <adam.schill.collberg@protonmail.com>
2021-12-22 16:58:49 +0100

    Optimize Graph object methods
    Only yield column of interest in each call.

--- gdsclient/graph/graph.py
+++ gdsclient/graph/graph.py
@@ -6,9 +6,11 @@ class Graph:
     def name(self):
         return self._name
 
-    def _graph_info(self):
+    def _graph_info(self, yields=[]):
+        yield_suffix = "" if len(yields) == 0 else " YIELD " + ", ".join(yields)
         info = self._query_runner.run_query(
-            "CALL gds.graph.list($graph_name)", {"graph_name": self._name}
+            f"CALL gds.graph.list($graph_name){yield_suffix}",
+            {"graph_name": self._name},
         )
 
         if len(info) == 0:
@@ -17,13 +19,13 @@ class Graph:
         return info[0]
 
     def node_count(self):
-        return self._graph_info()["nodeCount"]
+        return self._graph_info(["nodeCount"])["nodeCount"]
 
     def relationship_count(self):
-        return self._graph_info()["relationshipCount"]
+        return self._graph_info(["relationshipCount"])["relationshipCount"]
 
     def node_properties(self, label):
-        labels_to_props = self._graph_info()["schema"]["nodes"]
+        labels_to_props = self._graph_info(["schema"])["schema"]["nodes"]
         if label not in labels_to_props.keys():
             raise ValueError(
                 f"There is no node label '{label}' projected onto '{self.name()}'"
@@ -32,7 +34,7 @@ class Graph:
         return list(labels_to_props[label].keys())
 
     def relationship_properties(self, type):
-        types_to_props = self._graph_info()["schema"]["relationships"]
+        types_to_props = self._graph_info(["schema"])["schema"]["relationships"]
         if type not in types_to_props.keys():
             raise ValueError(
                 f"There is no relationship type '{type}' projected onto '{self.name()}'"
@@ -41,13 +43,13 @@ class Graph:
         return list(types_to_props[type].keys())
 
     def degree_distribution(self):
-        return self._graph_info()["degreeDistribution"]
+        return self._graph_info(["degreeDistribution"])["degreeDistribution"]
 
     def density(self):
-        return self._graph_info()["density"]
+        return self._graph_info(["density"])["density"]
 
     def memory_usage(self):
-        return self._graph_info()["memoryUsage"]
+        return self._graph_info(["memoryUsage"])["memoryUsage"]
 
     def size_in_bytes(self):
-        return self._graph_info()["sizeInBytes"]
+        return self._graph_info(["sizeInBytes"])["sizeInBytes"]
