66244db2573758281579bd6cb1c709992710534e
Adam Schill Collberg <adam.schill.collberg@protonmail.com>
2022-01-03 08:34:20 +0100

    Add initial mypy support

--- gdsclient/__init__.py
+++ gdsclient/__init__.py
@@ -1,4 +1,4 @@
 from .graph_data_science import GraphDataScience
 from .query_runner import Neo4jQueryRunner, QueryRunner
 
-__all__ = [GraphDataScience, QueryRunner, Neo4jQueryRunner]
+__all__ = ["GraphDataScience", "QueryRunner", "Neo4jQueryRunner"]
