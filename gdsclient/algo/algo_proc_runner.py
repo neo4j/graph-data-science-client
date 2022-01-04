27740a66b22bff432bf1b5a5233f9b609553f97d
Florentin Dörre <florentin.dorre@neotechnology.com>
2022-01-04 17:23:23 +0100

    WIP thoughts on path algos
    Co-authored-by: Adam Schill Collberg <adam.schill.collberg@protonmail.com>

--- gdsclient/algo/algo_proc_runner.py
+++ gdsclient/algo/algo_proc_runner.py
@@ -24,3 +24,31 @@ class AlgoProcRunner:
     def estimate(self, G: Graph, **config: Any) -> QueryResult:
         self._proc_name += "." + "estimate"
         return self._run_procedure(G, config)
+
+
+# Path algorithm support
+# for now: source and target node via previous cypher query / arbitrary id from 0..nodeCount 
+        # supported types: Integer / (mostly also) Node object
+# question: how does the user avoid the previous cypher query to get the node/node-id
+
+# user wants to match:
+#   single node
+#   list of nodes (BFS)
+
+# goal: convenience to match a source|target node(s)
+# ideas:
+#     some utility to avoid cypher queries
+#           find specific node for label + properties parameters (make sure its only a single node)
+#           arguments: list of labels, dictionary for parameters (how much do we want to rewrite the input into cypher?)
+#     should not be an operation over a given graph object YET (in the future when using cypher on gds)
+#     (a) could be a python wrapper specific function exposed the same as other gds functions
+#       such as: gds.util.nodes(labelFilter:str, **nodeProperties:Any) !? how do we show this is not part of the library but python wrapper specific
+#       one that expects only one result and another function that allows multiple (only needed by BFS for now)
+#     (b) allow execution of an arbitrary cypher query and we make sure the result is only a single field and we transform the result (simple & cheap)
+#           gds.findNode(cypherQuery:str) -> int | gds.findNodes(cypherQuery:str) -> list[int]
+#           Example: 
+#               gds.shortestPath.dijkstra.stream(
+#                   graph, 
+#                   sourceNode=gds.findNode("MATCH (source:Location {name: 'A'}) RETURN source"), 
+#                   targetNode=gds.findNode("MATCH (target:Location {name: 'F'}) RETURN target"), 
+#                   ...)
