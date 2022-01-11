6f2727b52e46740c3b35108573136b9db4ccf725
Mats Rydberg <mats@neotechnology.com>
2022-01-11 10:21:14 +0100

    Add test fixture for model management
    Co-authored-by: Adam Schill Collberg <adam.schill.collberg@protonmail.com>

--- gdsclient/tests/integration/test_graphsage.py
+++ gdsclient/tests/integration/test_graphsage.py
@@ -4,6 +4,7 @@ import pytest
 
 from gdsclient.graph.graph_object import Graph
 from gdsclient.graph_data_science import GraphDataScience
+from gdsclient.model.trained_model import GraphSageModel
 from gdsclient.query_runner.neo4j_query_runner import Neo4jQueryRunner
 
 MODEL_NAME = "gs"
@@ -33,39 +34,36 @@ def G(runner: Neo4jQueryRunner, gds: GraphDataScience) -> Generator[Graph, None,
     G.drop()
 
 
-def test_graphsage_train(G: Graph, gds: GraphDataScience) -> None:
-    model = gds.beta.graphSage.train(G, modelName="m", featureProperties=["x"])
+@pytest.fixture
+def model(gds: GraphDataScience, G: Graph) -> Generator[GraphSageModel, None, None]:
+    model = gds.beta.graphSage.train(
+        G, modelName="m", featureProperties=["x"], embeddingDimension=20
+    )
 
-    assert model.name() == "m"
-    assert model.exists()
+    yield model
 
     model.drop()
 
 
+def test_graphsage_train(model: GraphSageModel) -> None:
+    assert model.name() == "m"
+    assert model.exists()
+
+
 def test_graphsage_write(
-    G: Graph, gds: GraphDataScience, runner: Neo4jQueryRunner
+    G: Graph, model: GraphSageModel, runner: Neo4jQueryRunner
 ) -> None:
-    model = gds.beta.graphSage.train(
-        G, modelName="m", featureProperties=["x"], embeddingDimension=20
-    )
     model.predict_write(G, writeProperty="gs")
 
     result = runner.run_query("MATCH (n:Node) RETURN size(n.gs) AS embeddingDim")
     assert len(result) == G.node_count()
     assert result[0]["embeddingDim"] == 20
 
-    model.drop()
-
 
 def test_graphsage_stream(
-    G: Graph, gds: GraphDataScience, runner: Neo4jQueryRunner
+    G: Graph, model: GraphSageModel, runner: Neo4jQueryRunner
 ) -> None:
-    model = gds.beta.graphSage.train(
-        G, modelName="m", featureProperties=["x"], embeddingDimension=20
-    )
     stream = model.predict_stream(G)
 
     assert len(stream) == G.node_count()
     assert len(stream[0]["embedding"]) == 20
-
-    model.drop()
