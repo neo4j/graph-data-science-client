887c0475b3c4b09d21475c05fa56e9b6765917a3
Adam Schill Collberg <adam.schill.collberg@protonmail.com>
2022-01-19 10:37:53 +0100

    Support similarity procedures for current namespaces

--- gdsclient/similarity/similarity_endpoints.py
+++ gdsclient/similarity/similarity_endpoints.py
@@ -1,30 +1,16 @@
-from typing import List
-
 from ..query_runner.query_runner import QueryRunner
+from .similarity_algo_runner import SimilarityAlgoRunner
 
 
 class SimilarityEndpoints:
-    # NOTE: We cannot use "similarity" as an endpoint in this class
-    # since that will clash with e.g. `gds.alpha.similarity.cosine.write`
-    # which should trigger "write" of the AlgoEndpoints.
-
     def __init__(self, query_runner: QueryRunner, namespace: str):
         self._query_runner = query_runner
         self._namespace = namespace
 
-    def _run_function(
-        self, namespace: str, vector1: List[int], vector2: List[int]
-    ) -> float:
-        result = self._query_runner.run_query(
-            f"RETURN {namespace}({vector1}, {vector2}) AS similarity"
-        )
+    @property
+    def jaccard(self) -> SimilarityAlgoRunner:
+        return SimilarityAlgoRunner(self._query_runner, f"{self._namespace}.jaccard")
 
-        return result[0]["similarity"]  # type: ignore
-
-    def jaccard(self, vector1: List[int], vector2: List[int]) -> float:
-        namespace = self._namespace + ".jaccard"
-        return self._run_function(namespace, vector1, vector2)
-
-    def cosine(self, vector1: List[int], vector2: List[int]) -> float:
-        namespace = self._namespace + ".cosine"
-        return self._run_function(namespace, vector1, vector2)
+    @property
+    def cosine(self) -> SimilarityAlgoRunner:
+        return SimilarityAlgoRunner(self._query_runner, f"{self._namespace}.cosine")
--- gdsclient/tests/integration/test_similarity.py
+++ gdsclient/tests/integration/test_similarity.py
@@ -11,3 +11,11 @@ def test_similarity_jaccard(gds: GraphDataScience) -> None:
 def test_similarity_cosine(gds: GraphDataScience) -> None:
     result = gds.alpha.similarity.cosine([3, 8, 7, 5, 2, 9], [10, 8, 6, 6, 4, 5])
     assert result == pytest.approx(0.86, 0.01)
+
+
+def test_similarity_cosine_stats(gds: GraphDataScience) -> None:
+    node1 = {"item": 1, "weights": [42.0, 13.37]}
+    node2 = {"item": 2, "weights": [13.37, 42]}
+
+    result = gds.alpha.similarity.cosine.stats(data=[node1, node2])
+    assert result[0]["nodes"] == 2
