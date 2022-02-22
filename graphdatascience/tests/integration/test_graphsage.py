4c3c54ccb4810d8857aee3726e24220dbb6ce300
Adam Schill Collberg <adam.schill.collberg@protonmail.com>
2022-02-22 12:56:16 +0100

    Let GraphSage train also return result row

--- graphdatascience/tests/integration/test_graphsage.py
+++ graphdatascience/tests/integration/test_graphsage.py
@@ -36,11 +36,12 @@ def G(runner: Neo4jQueryRunner, gds: GraphDataScience) -> Generator[Graph, None,
 
 @pytest.fixture
 def model(gds: GraphDataScience, G: Graph) -> Generator[GraphSageModel, None, None]:
-    model = gds.beta.graphSage.train(
+    model, _ = gds.beta.graphSage.train(
         G, modelName="m", featureProperties=["x"], embeddingDimension=20
     )
 
-    yield model
+    # mypy bug
+    yield model  # type: ignore
 
     model.drop()
 
