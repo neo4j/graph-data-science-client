e5a9b39054c3313537ae6cec5de04c2eacf86898
Adam Schill Collberg <adam.schill.collberg@protonmail.com>
2022-03-16 18:03:35 +0100

    Align simple algo and graph with to_df()
    Co-Authored-By: Sören Reichardt <soren.reichardt@neotechnology.com>

--- graphdatascience/graph/graph_export_runner.py
+++ graphdatascience/graph/graph_export_runner.py
@@ -1,7 +1,9 @@
 from typing import Any, Dict
 
+from pandas.core.series import Series
+
 from ..error.illegal_attr_checker import IllegalAttrChecker
-from ..query_runner.query_runner import QueryRunner, Row
+from ..query_runner.query_runner import QueryRunner
 from .graph_object import Graph
 
 
@@ -10,17 +12,17 @@ class GraphExportRunner(IllegalAttrChecker):
         self._query_runner = query_runner
         self._namespace = namespace
 
-    def __call__(self, G: Graph, **config: Any) -> Row:
+    def __call__(self, G: Graph, **config: Any) -> Series:
         return self._export_call(G, config)
 
-    def _export_call(self, G: Graph, config: Dict[str, Any]) -> Row:
+    def _export_call(self, G: Graph, config: Dict[str, Any]) -> Series:
         query = f"CALL {self._namespace}($graph_name, $config)"
         params = {"graph_name": G.name(), "config": config}
 
-        return self._query_runner.run_query(query, params)[0]
+        return self._query_runner.run_query(query, params).squeeze()
 
     # TODO: Add an integration test for this call.
-    def csv(self, G: Graph, **config: Any) -> Row:
+    def csv(self, G: Graph, **config: Any) -> Series:
         self._namespace += ".csv"
 
         return self._export_call(G, config)
