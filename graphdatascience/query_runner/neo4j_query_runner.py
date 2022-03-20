503e69002a483fec0a7096298812be41f13ddae3
Adam Schill Collberg <adam.schill.collberg@protonmail.com>
2022-03-20 13:08:07 +0100

    Hide neo4j warning of to_df() being experimental
    Since we should henceforth for the foreseeable future provide
    pandas types return values whenever appropriate we should not
    warn that's it's experimental in neo4j.
    If the neo4j API is change in terms of to_df() we will fix it
    internally and the user should not have to worry about things
    being experimental in certain packages.
    
    TL;DR: pandas support is not experimental in graphdatascience.

--- graphdatascience/query_runner/neo4j_query_runner.py
+++ graphdatascience/query_runner/neo4j_query_runner.py
@@ -1,3 +1,4 @@
+import warnings
 from typing import Any, Dict
 
 import neo4j
@@ -14,6 +15,14 @@ class Neo4jQueryRunner(QueryRunner):
     def run_query(self, query: str, params: Dict[str, Any] = {}) -> DataFrame:
         with self._driver.session(database=self._db) as session:
             result = session.run(query, params)
+
+            # Though pandas support may be experimental in the `neo4j` package, it should always
+            # be supported in the `graphdatascience` package.
+            warnings.filterwarnings(
+                "ignore",
+                message=r"^pandas support is experimental and might be changed or removed in future versions$",
+            )
+
             return result.to_df()
 
     def set_database(self, db: str) -> None:
