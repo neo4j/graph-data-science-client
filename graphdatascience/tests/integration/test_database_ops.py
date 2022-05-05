566ab244588ee31a8cdf2eff82352b5127e2a364
Adam Schill Collberg <adam.schill.collberg@protonmail.com>
2022-05-05 14:19:53 +0200

    Update switching database test

--- graphdatascience/tests/integration/test_database_ops.py
+++ graphdatascience/tests/integration/test_database_ops.py
@@ -1,7 +1,7 @@
 import re
 
 import pytest
-from neo4j import DEFAULT_DATABASE, Driver
+from neo4j import Driver
 
 from graphdatascience.error.unable_to_connect import UnableToConnectError
 from graphdatascience.graph_data_science import GraphDataScience
@@ -13,6 +13,7 @@ GRAPH_NAME = "g"
 
 
 def test_switching_db(runner: Neo4jQueryRunner) -> None:
+    default_database = runner.database()
     runner.run_query("CREATE (a: Node)")
 
     pre_count = runner.run_query("MATCH (n: Node) RETURN COUNT(n) AS c")["c"][0]
@@ -25,7 +26,7 @@ def test_switching_db(runner: Neo4jQueryRunner) -> None:
     post_count = runner.run_query("MATCH (n: Node) RETURN COUNT(n) AS c")["c"][0]
     assert post_count == 0
 
-    runner.set_database(DEFAULT_DATABASE)
+    runner.set_database(default_database)
     runner.run_query("MATCH (n) DETACH DELETE n")
     runner.run_query("DROP DATABASE $dbName", {"dbName": MY_DB_NAME})
 
