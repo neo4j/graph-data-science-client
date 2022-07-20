513dcf7042dbb433eced974ef9b97da1aafbc9fc
Adam Schill Collberg <adam.schill.collberg@protonmail.com>
2022-07-20 10:49:33 +0200

    Drop support for Python 3.6
    Since it has been end-of-life for a while now and no longer receiving
    security updates.

--- setup.py
+++ setup.py
@@ -17,7 +17,6 @@ classifiers = [
     "Operating System :: OS Independent",
     "Programming Language :: Python :: 3",
     "Programming Language :: Python :: 3 :: Only",
-    "Programming Language :: Python :: 3.6",
     "Programming Language :: Python :: 3.7",
     "Programming Language :: Python :: 3.8",
     "Programming Language :: Python :: 3.9",
@@ -48,7 +47,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     package_data={"graphdatascience": ["py.typed"]},
     project_urls=project_urls,
-    python_requires=">=3.6",
+    python_requires=">=3.7",
     install_requires=reqs,
     zip_safe=False,
 )
