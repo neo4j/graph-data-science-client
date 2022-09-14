87f4d48e42dbd0a36d6cc0a9c7ea8a2546105947
Adam Schill Collberg <adam.schill.collberg@protonmail.com>
2022-09-14 17:14:44 +0200

    Package pickle resource files

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setuptools.setup(
     url="https://neo4j.com/product/graph-data-science/",
     classifiers=classifiers,
     packages=setuptools.find_packages(),
-    package_data={"graphdatascience": ["py.typed"]},
+    package_data={"graphdatascience": ["py.typed", "resources/**/*.pkl"]},
     project_urls=project_urls,
     python_requires=">=3.7",
     install_requires=reqs,
