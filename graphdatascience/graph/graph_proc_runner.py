8cd4429ed7aef534065c0504bafcc763ed1c30e0
Brian Shi <brian.shi@neotechnology.com>
2022-11-24 10:32:53 +0000

    Add imdb pickle and loader

--- graphdatascience/graph/graph_proc_runner.py
+++ graphdatascience/graph/graph_proc_runner.py
@@ -61,6 +61,49 @@ class GraphProcRunner(CallerBase, UncallableNamespace, IllegalAttrChecker):
 
         return self.construct(graph_name, nodes, rels, undirected_relationship_types=undirected_relationship_types)
 
+    def load_imdb(self, graph_name: str = "cora") -> Graph:
+        with path("graphdatascience.resources.imdb", "labels.pkl") as labels_resource:
+            class_labels = read_pickle(labels_resource)
+        movies = pd.DataFrame([item for sublist in class_labels for item in sublist])
+        movies = movies.rename(columns={0: "nodeId", 1: "class"})
+        movies["labels"] = "Movie"
+
+        with path("graphdatascience.resources.imdb", "node_features.pkl") as features_resource:
+            raw_features = read_pickle(features_resource)
+        nodes_features_df = pd.DataFrame(raw_features)
+        nodes_features_df["feature"] = nodes_features_df.values.tolist()
+        nodes_features_df = nodes_features_df["feature"].reset_index().rename(columns={"index": "nodeId"})
+
+        movie_nodes_with_features = movies.merge(nodes_features_df, on="nodeId", how="left")
+        person_nodes_with_features = (
+            pd.merge(nodes_features_df, movies, on=["nodeId"], how="left", indicator=True)
+            .query('_merge=="left_only"')
+            .drop(columns=["class", "_merge"])
+        )
+        person_nodes_with_features["labels"] = "Person"
+
+        with path("graphdatascience.resources.imdb", "edges.pkl") as rels_resource:
+            spmatrices = read_pickle(rels_resource)
+
+        edge_list = []
+        for spmatrix in spmatrices:
+            raw_adj_matrix = pd.DataFrame.sparse.from_spmatrix(spmatrix).stack().reset_index()  # type: ignore
+            adj_matrix = raw_adj_matrix[raw_adj_matrix[0] != 0]
+            edge_list.append(
+                adj_matrix.iloc[:, :-1].rename(columns={"level_0": "sourceNodeId", "level_1": "targetNodeId"})
+            )
+
+        edge_list[0]["relationshipType"] = "MovieDirector"
+        edge_list[1]["relationshipType"] = "MovieDirector"
+        edge_list[2]["relationshipType"] = "MovieActor"
+        edge_list[3]["relationshipType"] = "MovieActor"
+
+        labeled_rels = pd.concat(edge_list)
+
+        self._namespace = "gds.alpha.graph"
+
+        return self.construct(graph_name, [movie_nodes_with_features, person_nodes_with_features], labeled_rels)
+
     @property
     def project(self) -> GraphProjectRunner:
         self._namespace += ".project"
