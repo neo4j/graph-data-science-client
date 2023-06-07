9a6e41253ca3aad882750672df143db998c05491
Max Kießling <max.kiessling@neotechnology.com>
2023-06-07 11:40:34 +0200

    Rename test and enable it on aura

--- graphdatascience/tests/integration/test_system_ops.py
+++ graphdatascience/tests/integration/test_system_ops.py
@@ -29,12 +29,10 @@ def test_sysInfo(gds: GraphDataScience) -> None:
 
     assert "gdsVersion" in (list(result["key"]))
 
-@pytest.mark.skip_on_aura
-def test_sysInfo(gds: GraphDataScience) -> None:
+def test_debug_arrow(gds: GraphDataScience) -> None:
     result = gds.debug.arrow()
 
     assert "listenAddress" in (list(result["key"]))
-
 
 @pytest.mark.enterprise
 def test_is_licensed(gds: GraphDataScience) -> None:
