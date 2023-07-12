ae2af59363f15071be143fd0185f17c3da964564
Brian Shi <brian.shi@neotechnology.com>
2023-07-12 17:10:01 +0100

    Add all configs to CRD
    Co-authored-by: Florentin Dörre <florentin.dorre@neo4j.com>
    Co-authored-by: Olga Razvenskaia <olga.razvenskaia@neo4j.com>

--- graphdatascience/gnn/gnn_nc_runner.py
+++ graphdatascience/gnn/gnn_nc_runner.py
@@ -12,23 +12,22 @@ class GNNNodeClassificationRunner(UncallableNamespace, IllegalAttrChecker):
             "featureProperties": feature_properties,
             "targetProperty": target_property,
             "job_type": "train",
+            "nodeProperties": feature_properties + [target_property]
         }
 
-        node_properties = feature_properties + [target_property]
         if target_node_label:
             configMap["targetNodeLabel"] = target_node_label
+        if node_labels:
+            configMap["nodeLabels"] = node_labels
+
         mlTrainingConfig = json.dumps(configMap)
-        # TODO query available node labels
-        node_labels = ["Paper"] if not node_labels else node_labels
 
         # token and uri will be injected by arrow_query_runner
         self._query_runner.run_query(
             f"CALL gds.upload.graph($graph_name, $config)", 
             params={"graph_name": graph_name, "config": {
                      "mlTrainingConfig": mlTrainingConfig, 
-                     "modelName": model_name, 
-                     "nodeLabels": node_labels,
-                    "nodeProperties": node_properties
+                     "modelName": model_name
                     }}
                     )
 
