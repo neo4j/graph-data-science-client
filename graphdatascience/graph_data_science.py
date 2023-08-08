6de310bd10ec88f25183b23b5907cfa7a6057d8e
Florentin Dörre <florentin.dorre@neo4j.com>
2023-08-08 14:45:38 +0200

    Avoid using deprecated YIELD in init

--- graphdatascience/graph_data_science.py
+++ graphdatascience/graph_data_science.py
@@ -103,8 +103,9 @@ class GraphDataScience(DirectEndpoints, UncallableNamespace):
         self._query_runner.set_server_version(self._server_version)
 
         if arrow and self._server_version >= ServerVersion(2, 1, 0):
+            yield_fields = "running, listenAddress" if self._server_version >= ServerVersion(2,2,1) else "running, advertisedListenAddress"
             arrow_info: "Series[Any]" = self._query_runner.run_query(
-                "CALL gds.debug.arrow()", custom_error=False
+               f"CALL gds.debug.arrow() YIELD {yield_fields}", custom_error=False
             ).squeeze()
             listen_address: str = arrow_info.get("advertisedListenAddress", arrow_info["listenAddress"])  # type: ignore
             if arrow_info["running"]:
