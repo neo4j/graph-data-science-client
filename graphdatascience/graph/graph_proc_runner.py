e14cf13f0188d4214cac77331157b837c896bfd8
Brian Shi <brian.shi@neotechnology.com>
2023-08-14 14:29:41 +0100

    Remove hetero_tag_rel option for lastfm

--- graphdatascience/graph/graph_proc_runner.py
+++ graphdatascience/graph/graph_proc_runner.py
@@ -156,7 +156,7 @@ class GraphProcRunner(UncallableNamespace, IllegalAttrChecker):
         return self.construct(graph_name, nodes, rels, undirected_relationship_types=undirected_relationship_types)
 
     @client_only_endpoint("gds.graph")
-    def load_lastfm(self, graph_name: str = "lastfm", undirected: bool = True, hetero_tag_rel: bool = False) -> Any:
+    def load_lastfm(self, graph_name: str = "lastfm", undirected: bool = True) -> Any:
         if self._server_version < ServerVersion(2, 3, 0):
             raise ValueError("The LastFM2K dataset loading is only supported by GDS 2.3 or later.")
 
@@ -169,26 +169,15 @@ class GraphProcRunner(UncallableNamespace, IllegalAttrChecker):
             user_friend_df_directed = read_pickle(rels_resource, compression="gzip")
         with self._path("graphdatascience.resources.lastfm", "user_listen_artist_rels.pkl") as rels_resource:
             user_listen_artist_rels = read_pickle(rels_resource, compression="gzip")
-        if hetero_tag_rel:
-            with self._path("graphdatascience.resources.lastfm", "user_tag_artist_rels_hetero.pkl") as rels_resource:
-                user_tag_artist_rels = read_pickle(rels_resource, compression="gzip")
-        else:
-            with self._path("graphdatascience.resources.lastfm", "user_tag_artist_rels.pkl") as rels_resource:
-                user_tag_artist_rels = read_pickle(rels_resource, compression="gzip")
+        with self._path("graphdatascience.resources.lastfm", "user_tag_artist_rels.pkl") as rels_resource:
+            user_tag_artist_rels = read_pickle(rels_resource, compression="gzip")
 
         nodes = [user_nodes, artist_nodes]
         rels = [user_friend_df_directed, user_listen_artist_rels, user_tag_artist_rels]
 
         # Default undirected for usage in GDS ML pipelines
         if undirected:
-            if hetero_tag_rel:
-                # There are 9749 different tag ids (hence hetero-Tag rels)
-                tag_rels = user_tag_artist_rels[["relationshipType"]].drop_duplicates()
-                undirected_relationship_types = ["LISTEN_TO", "IS_FRIEND"] + tag_rels[
-                    "relationshipType"
-                ].values.tolist()
-            else:
-                undirected_relationship_types = ["LISTEN_TO", "TAGGED", "IS_FRIEND"]
+            undirected_relationship_types = ["LISTEN_TO", "TAGGED", "IS_FRIEND"]
         else:
             undirected_relationship_types = []
 
