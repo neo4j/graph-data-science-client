8de824848ece5e3446d6a6b36fc6ac7dc04c54d5
Adam Schill Collberg <adam.schill.collberg@protonmail.com>
2023-09-06 09:47:00 +0200

    Update edge embeddings API to server API

--- graphdatascience/model/simple_edge_embedding_model.py
+++ graphdatascience/model/simple_edge_embedding_model.py
@@ -25,46 +25,42 @@ class SimpleEdgeEmbeddingModel:
 
     def predict_stream(
         self,
-        source_node_spec: Union[str, List[int]],
-        target_node_spec: Union[str, List[int]],
+        source_node_filter: Union[str, List[int]],
+        target_node_filter: Union[str, List[int]],
         relationship_type: str,
         top_k: int,
-        threshold: float,
     ) -> DataFrame:
         return self._query_runner.run_query(
             """
             CALL gds.ml.kge.predict.stream(
                 $graph_name,
                 {
-                    sourceNodeSpec: $source_node_spec
-                    targetNodeSpec: $target_node_spec
-                    nodeEmbeddingProperty: $node_embedding_property
-                    relationshipTypeEmbedding: $relationship_type_embedding
+                    sourceNodeFilter: $source_node_filter,
+                    targetNodeFilter: $target_node_filter,
+                    nodeEmbeddingProperty: $node_embedding_property,
+                    relationshipTypeEmbedding: $relationship_type_embedding,
                     scoringFunction: $scoring_function,
-                    topK: $top_k,
-                    threshold: $threshold
+                    topK: $top_k
                 }
             )
             """,
             {
                 "graph_name": self._graph_name,
-                "source_node_spec": source_node_spec,
-                "target_node_spec": target_node_spec,
+                "source_node_filter": source_node_filter,
+                "target_node_filter": target_node_filter,
                 "node_embedding_property": self._node_embedding_property,
                 "relationship_type_embedding": self._relationship_type_embeddings[relationship_type],
                 "scoring_function": self._scoring_function,
                 "top_k": top_k,
-                "threshold": threshold,
             },
         )
 
     def predict_mutate(
         self,
-        source_node_spec: Union[str, List[int]],
-        target_node_spec: Union[str, List[int]],
+        source_node_filter: Union[str, List[int]],
+        target_node_filter: Union[str, List[int]],
         relationship_type: str,
         top_k: int,
-        threshold: float,
         mutate_relationship_type: str,
     ) -> "Series[Any]":
         return self._query_runner.run_query(  # type: ignore
@@ -72,64 +68,59 @@ class SimpleEdgeEmbeddingModel:
             CALL gds.ml.kge.predict.mutate(
                 $graph_name,
                 {
-                    sourceNodeSpec: $source_node_spec
-                    targetNodeSpec: $target_node_spec
-                    nodeEmbeddingProperty: $node_embedding_property
-                    relationshipTypeEmbedding: $relationship_type_embedding
+                    sourceNodeFilter: $source_node_filter,
+                    targetNodeFilter: $target_node_filter,
+                    nodeEmbeddingProperty: $node_embedding_property,
+                    relationshipTypeEmbedding: $relationship_type_embedding,
                     scoringFunction: $scoring_function,
                     topK: $top_k,
-                    threshold: $threshold,
                     mutateRelationshipType: $mutate_relationship_type
                 }
             )
             """,
             {
                 "graph_name": self._graph_name,
-                "source_node_spec": source_node_spec,
-                "target_node_spec": target_node_spec,
+                "source_node_filter": source_node_filter,
+                "target_node_filter": target_node_filter,
                 "node_embedding_property": self._node_embedding_property,
                 "relationship_type_embedding": self._relationship_type_embeddings[relationship_type],
                 "scoring_function": self._scoring_function,
                 "top_k": top_k,
-                "threshold": threshold,
                 "mutate_relationship_type": mutate_relationship_type,
             },
         ).squeeze()
 
     def predict_write(
         self,
-        source_node_spec: Union[str, List[int]],
-        target_node_spec: Union[str, List[int]],
+        source_node_filter: Union[str, List[int]],
+        target_node_filter: Union[str, List[int]],
         relationship_type: str,
         top_k: int,
-        threshold: float,
         write_relationship_type: str,
     ) -> "Series[Any]":
         return self._query_runner.run_query(  # type: ignore
             """
-            CALL gds.ml.kge.predict.mutate(
+            CALL gds.ml.kge.predict.write(
                 $graph_name,
                 {
-                    sourceNodeSpec: $source_node_spec
-                    targetNodeSpec: $target_node_spec
-                    nodeEmbeddingProperty: $node_embedding_property
-                    relationshipTypeEmbedding: $relationship_type_embedding
+                    sourceNodeFilter: $source_node_filter,
+                    targetNodeFilter: $target_node_filter,
+                    nodeEmbeddingProperty: $node_embedding_property,
+                    relationshipTypeEmbedding: $relationship_type_embedding,
                     scoringFunction: $scoring_function,
                     topK: $top_k,
-                    threshold: $threshold,
                     writeRelationshipType: $write_relationship_type
                 }
             )
             """,
             {
                 "graph_name": self._graph_name,
-                "source_node_spec": source_node_spec,
-                "target_node_spec": target_node_spec,
+                "source_node_filter": source_node_filter,
+                "target_node_filter": target_node_filter,
                 "node_embedding_property": self._node_embedding_property,
                 "relationship_type_embedding": self._relationship_type_embeddings[relationship_type],
                 "scoring_function": self._scoring_function,
                 "top_k": top_k,
-                "threshold": threshold,
                 "write_relationship_type": write_relationship_type,
             },
         ).squeeze()
--- graphdatascience/tests/unit/test_edge_embedding_models.py
+++ graphdatascience/tests/unit/test_edge_embedding_models.py
@@ -10,10 +10,9 @@ NODE_PROP = "dummy_prop"
 REL_TYPE = "dummy_type"
 REL_TYPE_EMBEDDING = [1.0, 2.0, 3.0]
 WRITE_MUTATE_REL_TYPE = "another_dummy_type"
-SOURCE_NODE_SPEC = "dummy_source_spec"
-TARGET_NODE_SPEC = "dummy_target_spec"
+SOURCE_NODE_FILTER = "dummy_source_spec"
+TARGET_NODE_FILTER = "dummy_target_spec"
 TOP_K = 10
-THRESHOLD = 0.5
 
 
 @pytest.fixture
@@ -33,82 +32,77 @@ def distmult_M(gds: GraphDataScience, G: Graph) -> SimpleEdgeEmbeddingModel:
 
 
 def test_transe_predict_stream(runner: CollectingQueryRunner, transe_M: SimpleEdgeEmbeddingModel) -> None:
-    _ = transe_M.predict_stream(SOURCE_NODE_SPEC, TARGET_NODE_SPEC, REL_TYPE, TOP_K, THRESHOLD)
+    _ = transe_M.predict_stream(SOURCE_NODE_FILTER, TARGET_NODE_FILTER, REL_TYPE, TOP_K)
 
     assert runner.last_query() == (
         """
             CALL gds.ml.kge.predict.stream(
                 $graph_name,
                 {
-                    sourceNodeSpec: $source_node_spec
-                    targetNodeSpec: $target_node_spec
-                    nodeEmbeddingProperty: $node_embedding_property
-                    relationshipTypeEmbedding: $relationship_type_embedding
+                    sourceNodeFilter: $source_node_filter,
+                    targetNodeFilter: $target_node_filter,
+                    nodeEmbeddingProperty: $node_embedding_property,
+                    relationshipTypeEmbedding: $relationship_type_embedding,
                     scoringFunction: $scoring_function,
-                    topK: $top_k,
-                    threshold: $threshold
+                    topK: $top_k
                 }
             )
             """
     )
     assert runner.last_params() == {
         "graph_name": GRAPH_NAME,
-        "source_node_spec": SOURCE_NODE_SPEC,
-        "target_node_spec": TARGET_NODE_SPEC,
+        "source_node_filter": SOURCE_NODE_FILTER,
+        "target_node_filter": TARGET_NODE_FILTER,
         "node_embedding_property": NODE_PROP,
         "relationship_type_embedding": REL_TYPE_EMBEDDING,
         "scoring_function": "transe",
         "top_k": TOP_K,
-        "threshold": THRESHOLD,
     }
 
 
 def test_distmult_predict_stream(runner: CollectingQueryRunner, distmult_M: SimpleEdgeEmbeddingModel) -> None:
-    _ = distmult_M.predict_stream(SOURCE_NODE_SPEC, TARGET_NODE_SPEC, REL_TYPE, TOP_K, THRESHOLD)
+    _ = distmult_M.predict_stream(SOURCE_NODE_FILTER, TARGET_NODE_FILTER, REL_TYPE, TOP_K)
 
     assert runner.last_query() == (
         """
             CALL gds.ml.kge.predict.stream(
                 $graph_name,
                 {
-                    sourceNodeSpec: $source_node_spec
-                    targetNodeSpec: $target_node_spec
-                    nodeEmbeddingProperty: $node_embedding_property
-                    relationshipTypeEmbedding: $relationship_type_embedding
+                    sourceNodeFilter: $source_node_filter,
+                    targetNodeFilter: $target_node_filter,
+                    nodeEmbeddingProperty: $node_embedding_property,
+                    relationshipTypeEmbedding: $relationship_type_embedding,
                     scoringFunction: $scoring_function,
-                    topK: $top_k,
-                    threshold: $threshold
+                    topK: $top_k
                 }
             )
             """
     )
     assert runner.last_params() == {
         "graph_name": GRAPH_NAME,
-        "source_node_spec": SOURCE_NODE_SPEC,
-        "target_node_spec": TARGET_NODE_SPEC,
+        "source_node_filter": SOURCE_NODE_FILTER,
+        "target_node_filter": TARGET_NODE_FILTER,
         "node_embedding_property": NODE_PROP,
         "relationship_type_embedding": REL_TYPE_EMBEDDING,
         "scoring_function": "distmult",
         "top_k": TOP_K,
-        "threshold": THRESHOLD,
     }
 
 
 def test_transe_predict_mutate(runner: CollectingQueryRunner, transe_M: SimpleEdgeEmbeddingModel) -> None:
-    _ = transe_M.predict_mutate(SOURCE_NODE_SPEC, TARGET_NODE_SPEC, REL_TYPE, TOP_K, THRESHOLD, WRITE_MUTATE_REL_TYPE)
+    _ = transe_M.predict_mutate(SOURCE_NODE_FILTER, TARGET_NODE_FILTER, REL_TYPE, TOP_K, WRITE_MUTATE_REL_TYPE)
 
     assert runner.last_query() == (
         """
             CALL gds.ml.kge.predict.mutate(
                 $graph_name,
                 {
-                    sourceNodeSpec: $source_node_spec
-                    targetNodeSpec: $target_node_spec
-                    nodeEmbeddingProperty: $node_embedding_property
-                    relationshipTypeEmbedding: $relationship_type_embedding
+                    sourceNodeFilter: $source_node_filter,
+                    targetNodeFilter: $target_node_filter,
+                    nodeEmbeddingProperty: $node_embedding_property,
+                    relationshipTypeEmbedding: $relationship_type_embedding,
                     scoringFunction: $scoring_function,
                     topK: $top_k,
-                    threshold: $threshold,
                     mutateRelationshipType: $mutate_relationship_type
                 }
             )
@@ -116,32 +110,30 @@ def test_transe_predict_mutate(runner: CollectingQueryRunner, transe_M: SimpleEd
     )
     assert runner.last_params() == {
         "graph_name": GRAPH_NAME,
-        "source_node_spec": SOURCE_NODE_SPEC,
-        "target_node_spec": TARGET_NODE_SPEC,
+        "source_node_filter": SOURCE_NODE_FILTER,
+        "target_node_filter": TARGET_NODE_FILTER,
         "node_embedding_property": NODE_PROP,
         "relationship_type_embedding": REL_TYPE_EMBEDDING,
         "scoring_function": "transe",
         "top_k": TOP_K,
-        "threshold": THRESHOLD,
         "mutate_relationship_type": WRITE_MUTATE_REL_TYPE,
     }
 
 
 def test_distmult_predict_mutate(runner: CollectingQueryRunner, distmult_M: SimpleEdgeEmbeddingModel) -> None:
-    _ = distmult_M.predict_mutate(SOURCE_NODE_SPEC, TARGET_NODE_SPEC, REL_TYPE, TOP_K, THRESHOLD, WRITE_MUTATE_REL_TYPE)
+    _ = distmult_M.predict_mutate(SOURCE_NODE_FILTER, TARGET_NODE_FILTER, REL_TYPE, TOP_K, WRITE_MUTATE_REL_TYPE)
 
     assert runner.last_query() == (
         """
             CALL gds.ml.kge.predict.mutate(
                 $graph_name,
                 {
-                    sourceNodeSpec: $source_node_spec
-                    targetNodeSpec: $target_node_spec
-                    nodeEmbeddingProperty: $node_embedding_property
-                    relationshipTypeEmbedding: $relationship_type_embedding
+                    sourceNodeFilter: $source_node_filter,
+                    targetNodeFilter: $target_node_filter,
+                    nodeEmbeddingProperty: $node_embedding_property,
+                    relationshipTypeEmbedding: $relationship_type_embedding,
                     scoringFunction: $scoring_function,
                     topK: $top_k,
-                    threshold: $threshold,
                     mutateRelationshipType: $mutate_relationship_type
                 }
             )
@@ -149,32 +141,30 @@ def test_distmult_predict_mutate(runner: CollectingQueryRunner, distmult_M: Simp
     )
     assert runner.last_params() == {
         "graph_name": GRAPH_NAME,
-        "source_node_spec": SOURCE_NODE_SPEC,
-        "target_node_spec": TARGET_NODE_SPEC,
+        "source_node_filter": SOURCE_NODE_FILTER,
+        "target_node_filter": TARGET_NODE_FILTER,
         "node_embedding_property": NODE_PROP,
         "relationship_type_embedding": REL_TYPE_EMBEDDING,
         "scoring_function": "distmult",
         "top_k": TOP_K,
-        "threshold": THRESHOLD,
         "mutate_relationship_type": WRITE_MUTATE_REL_TYPE,
     }
 
 
 def test_transe_predict_write(runner: CollectingQueryRunner, transe_M: SimpleEdgeEmbeddingModel) -> None:
-    _ = transe_M.predict_write(SOURCE_NODE_SPEC, TARGET_NODE_SPEC, REL_TYPE, TOP_K, THRESHOLD, WRITE_MUTATE_REL_TYPE)
+    _ = transe_M.predict_write(SOURCE_NODE_FILTER, TARGET_NODE_FILTER, REL_TYPE, TOP_K, WRITE_MUTATE_REL_TYPE)
 
     assert runner.last_query() == (
         """
-            CALL gds.ml.kge.predict.mutate(
+            CALL gds.ml.kge.predict.write(
                 $graph_name,
                 {
-                    sourceNodeSpec: $source_node_spec
-                    targetNodeSpec: $target_node_spec
-                    nodeEmbeddingProperty: $node_embedding_property
-                    relationshipTypeEmbedding: $relationship_type_embedding
+                    sourceNodeFilter: $source_node_filter,
+                    targetNodeFilter: $target_node_filter,
+                    nodeEmbeddingProperty: $node_embedding_property,
+                    relationshipTypeEmbedding: $relationship_type_embedding,
                     scoringFunction: $scoring_function,
                     topK: $top_k,
-                    threshold: $threshold,
                     writeRelationshipType: $write_relationship_type
                 }
             )
@@ -182,32 +172,30 @@ def test_transe_predict_write(runner: CollectingQueryRunner, transe_M: SimpleEdg
     )
     assert runner.last_params() == {
         "graph_name": GRAPH_NAME,
-        "source_node_spec": SOURCE_NODE_SPEC,
-        "target_node_spec": TARGET_NODE_SPEC,
+        "source_node_filter": SOURCE_NODE_FILTER,
+        "target_node_filter": TARGET_NODE_FILTER,
         "node_embedding_property": NODE_PROP,
         "relationship_type_embedding": REL_TYPE_EMBEDDING,
         "scoring_function": "transe",
         "top_k": TOP_K,
-        "threshold": THRESHOLD,
         "write_relationship_type": WRITE_MUTATE_REL_TYPE,
     }
 
 
 def test_distmult_predict_write(runner: CollectingQueryRunner, distmult_M: SimpleEdgeEmbeddingModel) -> None:
-    _ = distmult_M.predict_write(SOURCE_NODE_SPEC, TARGET_NODE_SPEC, REL_TYPE, TOP_K, THRESHOLD, WRITE_MUTATE_REL_TYPE)
+    _ = distmult_M.predict_write(SOURCE_NODE_FILTER, TARGET_NODE_FILTER, REL_TYPE, TOP_K, WRITE_MUTATE_REL_TYPE)
 
     assert runner.last_query() == (
         """
-            CALL gds.ml.kge.predict.mutate(
+            CALL gds.ml.kge.predict.write(
                 $graph_name,
                 {
-                    sourceNodeSpec: $source_node_spec
-                    targetNodeSpec: $target_node_spec
-                    nodeEmbeddingProperty: $node_embedding_property
-                    relationshipTypeEmbedding: $relationship_type_embedding
+                    sourceNodeFilter: $source_node_filter,
+                    targetNodeFilter: $target_node_filter,
+                    nodeEmbeddingProperty: $node_embedding_property,
+                    relationshipTypeEmbedding: $relationship_type_embedding,
                     scoringFunction: $scoring_function,
                     topK: $top_k,
-                    threshold: $threshold,
                     writeRelationshipType: $write_relationship_type
                 }
             )
@@ -215,13 +203,12 @@ def test_distmult_predict_write(runner: CollectingQueryRunner, distmult_M: Simpl
     )
     assert runner.last_params() == {
         "graph_name": GRAPH_NAME,
-        "source_node_spec": SOURCE_NODE_SPEC,
-        "target_node_spec": TARGET_NODE_SPEC,
+        "source_node_filter": SOURCE_NODE_FILTER,
+        "target_node_filter": TARGET_NODE_FILTER,
         "node_embedding_property": NODE_PROP,
         "relationship_type_embedding": REL_TYPE_EMBEDDING,
         "scoring_function": "distmult",
         "top_k": TOP_K,
-        "threshold": THRESHOLD,
         "write_relationship_type": WRITE_MUTATE_REL_TYPE,
     }
 
