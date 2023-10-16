e8d2de6957402bbe547d14e44bc284cbdd198b70
Florentin Dörre <florentin.dorre@neo4j.com>
2023-10-16 15:31:52 +0200

    Add connection_url to create response
    + Format

--- graphdatascience/aura_api.py
+++ graphdatascience/aura_api.py
@@ -1,13 +1,11 @@
-
-
-from dataclasses import dataclass
 import logging
 import time
+from dataclasses import dataclass
 
 import requests as req
 
 
-class AuraApi():
+class AuraApi:
     # FIXME allow to insert other for dev purpose
     base_uri = "https://api.neo4j.io/v1"
 
@@ -20,11 +18,11 @@ class AuraApi():
         if self._token is None:
             self._token = self._update_token()
         return self._token.access_token
-    
+
     def create_instance(self, name: str) -> "InstanceCreateDetails":
         # TODO should give more control here
         data = {
-            "name":  name,
+            "name": name,
             "memory": "8GB",
             "version": "5",
             "region": "europe-west1",
@@ -36,86 +34,86 @@ class AuraApi():
         response = req.post(
             "https://api.neo4j.io/v1/instances",
             json=data,
-            headers={'Authorization': f'Bearer {self.__token()}'},
+            headers={"Authorization": f"Bearer {self.__token()}"},
         )
 
         response.raise_for_status()
 
-        return InstanceCreateDetails.fromJson(response.json()['data'])
-    
+        return InstanceCreateDetails.fromJson(response.json()["data"])
+
     def delete_instance(self, instance_id: str) -> "InstanceDetails":
         response = req.delete(
-            f'{AuraApi.base_uri}/instances/{instance_id}',
-            headers={'Authorization': f'Bearer {self.__token()}'},
+            f"{AuraApi.base_uri}/instances/{instance_id}",
+            headers={"Authorization": f"Bearer {self.__token()}"},
         )
 
         response.raise_for_status()
 
-        return InstanceSpecificDetails.fromJson(response.json()['data'])
-    
-    def list_instances(self, instance_id:str=None) -> ["InstanceDetails"]:
+        return InstanceSpecificDetails.fromJson(response.json()["data"])
+
+    def list_instances(self, instance_id: str = None) -> ["InstanceDetails"]:
         maybe_instance_id = f"/{instance_id}" if instance_id else ""
 
         response = req.get(
-            f'{AuraApi.base_uri}/instances{maybe_instance_id}',
-            headers={'Authorization': f'Bearer {self.__token()}'},
+            f"{AuraApi.base_uri}/instances{maybe_instance_id}",
+            headers={"Authorization": f"Bearer {self.__token()}"},
         )
 
         # TODO if the instance_id does not exist, we get a 404
         response.raise_for_status()
 
-        raw_data = response.json()['data']
+        raw_data = response.json()["data"]
 
         if instance_id:
             return InstanceSpecificDetails.fromJson(raw_data)
         else:
             return [InstanceDetails.fromJson(i) for i in raw_data]
-    
+
     def _get_tenant_id(self) -> str:
         response = req.get(
-            f'{AuraApi.base_uri}/tenants',
-            headers={'Authorization': f'Bearer {self.__token()}'},
+            f"{AuraApi.base_uri}/tenants",
+            headers={"Authorization": f"Bearer {self.__token()}"},
         )
         response.raise_for_status()
 
-        raw_data = response.json()['data']
-
+        raw_data = response.json()["data"]
 
         assert len(raw_data) == 1
 
-        return raw_data[0]['id']
-        
+        return raw_data[0]["id"]
+
     def _update_token(self) -> "AuraAuthToken":
         data = {
-        'grant_type': 'client_credentials',
+            "grant_type": "client_credentials",
         }
 
         self._logger.debug("Updating oauth token")
 
         response = req.post(
-            'https://api.neo4j.io/oauth/token', data=data, auth=(self._credentials[0], self._credentials[1])
+            "https://api.neo4j.io/oauth/token", data=data, auth=(self._credentials[0], self._credentials[1])
         )
 
         response.raise_for_status()
 
         return AuraAuthToken(response.json())
-    
 
-class AuraAuthToken():
+
+class AuraAuthToken:
     access_token: str
     expires_in: int
     token_type: str
 
     def __init__(self, json: dict) -> None:
-        self.access_token = json['access_token']
-        self.expires_at = int(time.time()) + json['expires_in']
-        self.token_type = json['token_type']
+        self.access_token = json["access_token"]
+        self.expires_at = int(time.time()) + json["expires_in"]
+        self.token_type = json["token_type"]
 
     def is_expired(self) -> bool:
         return self.expires_at >= int(time.time())
 
+
 @dataclass(repr=True)
-class InstanceDetails():
+class InstanceDetails:
     id: str
     name: str
     tenant_id: str
@@ -124,10 +122,10 @@ class InstanceDetails():
     @classmethod
     def fromJson(cls, json: dict) -> "InstanceDetails":
         return cls(
-            id=json['id'],
-            name=json['name'],
-            tenant_id=json['tenant_id'],
-            cloud_provider=json['cloud_provider'],
+            id=json["id"],
+            name=json["name"],
+            tenant_id=json["tenant_id"],
+            cloud_provider=json["cloud_provider"],
         )
 
 
@@ -140,27 +138,30 @@ class InstanceSpecificDetails(InstanceDetails):
     @classmethod
     def fromJson(cls, json: dict) -> "InstanceSpecificDetails":
         return cls(
-            id=json['id'],
-            name=json['name'],
-            tenant_id=json['tenant_id'],
-            cloud_provider=json['cloud_provider'],
-            status=json['status'],
-            connection_url=json.get('connection_url', ""),
-            memory=json['memory'],
+            id=json["id"],
+            name=json["name"],
+            tenant_id=json["tenant_id"],
+            cloud_provider=json["cloud_provider"],
+            status=json["status"],
+            connection_url=json.get("connection_url", ""),
+            memory=json["memory"],
         )
+
 
 @dataclass(repr=True)
 class InstanceCreateDetails(InstanceDetails):
     password: str
     username: str
+    connection_url: str
 
     @classmethod
     def fromJson(cls, json: dict) -> "InstanceCreateDetails":
         return cls(
-            id=json['id'],
-            name=json.get('name', ""),
-            tenant_id=json['tenant_id'],
-            cloud_provider=json['cloud_provider'],
-            password=json['password'],
-            username=json['username'],
+            id=json["id"],
+            name=json.get("name", ""),
+            tenant_id=json["tenant_id"],
+            cloud_provider=json["cloud_provider"],
+            password=json["password"],
+            username=json["username"],
+            connection_url=json["connection_url"],
         )
