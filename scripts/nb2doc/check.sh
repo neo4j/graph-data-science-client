b409a2600c9afe0817f34a3fba05d3a7d79b88fa
Nicola Vitucci <nicola.vitucci@neo4j.com>
2023-11-07 17:18:58 +0000

    Another attempt to fix nb2doc check script

--- scripts/nb2doc/check.sh
+++ scripts/nb2doc/check.sh
@@ -5,9 +5,9 @@ set -o nounset
 set -o pipefail
 set -o xtrace
 
-${PWD}/scripts/nb2doc/convert.sh
+./convert.sh
 
-if ! git diff --quiet ${PWD}/doc/modules/ROOT/pages/tutorials/;
+if ! git diff --quiet doc/modules/ROOT/pages/tutorials/;
   then
     echo "Please run /scripts/nb2doc/convert.sh to update docs"
     exit 1
