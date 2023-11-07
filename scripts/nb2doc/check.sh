e6e9d919a40e34971c90666822e1bd2519e152da
Nicola Vitucci <nicola.vitucci@neo4j.com>
2023-11-07 21:31:36 +0000

    Update git conf for TC

--- scripts/nb2doc/check.sh
+++ scripts/nb2doc/check.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 
 set -o errexit
-# set -o nounset
+set -o nounset
 set -o pipefail
 set -o xtrace
 
 ${PWD}/scripts/nb2doc/convert.sh
 
-echo $GIT_DIR
+# Without this, TeamCity complains with a
+# "warning: Not a git repository" error
 export GIT_DIR=${PWD}/.git
-echo $GIT_DIR
-
-echo $GIT_INDEX_FILE
-export GIT_INDEX_FILE=${PWD}/.git/index
-echo $GIT_INDEX_FILE
 
 if ! git diff --quiet ${PWD}/doc/modules/ROOT/pages/tutorials/
 then
