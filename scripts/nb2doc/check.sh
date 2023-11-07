d732d2d2449ac926bc8514dea5cb72ff3cdf5c87
Nicola Vitucci <nicola.vitucci@neo4j.com>
2023-11-07 21:17:02 +0000

    Try more git conf for TC

--- scripts/nb2doc/check.sh
+++ scripts/nb2doc/check.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
 set -o errexit
-set -o nounset
+# set -o nounset
 set -o pipefail
 set -o xtrace
 
-# TODO: remove
-echo $(ls -lad .git)
-echo $(which git)
-
 ${PWD}/scripts/nb2doc/convert.sh
 
-if ! $(which git) diff --quiet doc/modules/ROOT/pages/tutorials/;
-  then
-    echo "Please run /scripts/nb2doc/convert.sh to update docs"
-    exit 1
+echo $GIT_DIR
+export GIT_DIR=${PWD}/.git
+echo $GIT_DIR
+
+echo $GIT_INDEX_FILE
+export GIT_INDEX_FILE=${PWD}/.git/index
+echo $GIT_INDEX_FILE
+
+if ! git diff --quiet ${PWD}/doc/modules/ROOT/pages/tutorials/
+then
+  echo "Please run /scripts/nb2doc/convert.sh to update docs"
+  exit 1
 fi
 
 
