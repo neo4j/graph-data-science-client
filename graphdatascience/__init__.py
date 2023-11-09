0e5a35a73482a88f12ddaa315fade591caf5c123
Adam Schill Collberg <adam.schill.collberg@protonmail.com>
2023-11-09 11:12:07 +0100

    Export custom data types in API

--- graphdatascience/__init__.py
+++ graphdatascience/__init__.py
@@ -1,5 +1,34 @@
+from .graph.graph_create_result import GraphCreateResult
+from .graph.graph_object import Graph
 from .graph_data_science import GraphDataScience
+from .model.graphsage_model import GraphSageModel
+from .model.link_prediction_model import LinkFeature, LPModel
+from .model.node_classification_model import NCModel
+from .model.node_regression_model import NRModel
+from .model.pipeline_model import NodePropertyStep
+from .model.simple_rel_embedding_model import SimpleRelEmbeddingModel
+from .pipeline.lp_training_pipeline import LPTrainingPipeline
+from .pipeline.nc_training_pipeline import NCTrainingPipeline
+from .pipeline.nr_training_pipeline import NRTrainingPipeline
 from .query_runner.query_runner import QueryRunner
+from .server_version.server_version import ServerVersion
 from .version import __version__
 
-__all__ = ["GraphDataScience", "QueryRunner", "__version__"]
+__all__ = [
+    "GraphDataScience",
+    "QueryRunner",
+    "__version__",
+    "ServerVersion",
+    "Graph",
+    "GraphCreateResult",
+    "LPTrainingPipeline",
+    "NCTrainingPipeline",
+    "NRTrainingPipeline",
+    "NodePropertyStep",
+    "LinkFeature",
+    "LPModel",
+    "NCModel",
+    "NRModel",
+    "GraphSageModel",
+    "SimpleRelEmbeddingModel",
+]
