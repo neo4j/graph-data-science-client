c1690df37371870210be22976a52f7c5c8e89e51
Florentin Dörre <florentin.dorre@neo4j.com>
2024-01-30 15:34:45 +0100

    Filter out more pandas warnings

--- graphdatascience/tests/integration/test_graph_ops.py
+++ graphdatascience/tests/integration/test_graph_ops.py
@@ -746,6 +746,13 @@ def test_graph_relationships_stream_without_arrow(gds_without_arrow: GraphDataSc
         "MATCH (n)-[r]->(m) RETURN id(n) AS src_id, id(m) AS trg_id, type(r) AS rel_type"
     )
 
+    # Pandas 2.2.0 deprecated an API used by the following assertion (in the underlying impl of pandas)
+    warnings.filterwarnings(
+            "ignore",
+            category=DeprecationWarning,
+            message=r"Passing a BlockManager to DataFrame is deprecated",
+    )
+
     assert result.shape[0] == expected.shape[0]
     for _, row in expected.iterrows():
         assert (result == np.array(row)).all(1).any()
