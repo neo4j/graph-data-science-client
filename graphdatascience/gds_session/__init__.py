bdcdcb871f45c258a6193e0f4c2bbb668685e0eb
Florentin Dörre <florentin.dorre@neo4j.com>
2024-02-14 16:37:32 +0100

    Fix checkstyle

--- graphdatascience/gds_session/__init__.py
+++ graphdatascience/gds_session/__init__.py
@@ -1,13 +1,11 @@
-from .gds_sessions import GdsSessions, SessionInfo, AuraAPICredentials
 from .dbms_connection_info import DbmsConnectionInfo
+from .gds_sessions import AuraAPICredentials, GdsSessions, SessionInfo
 from .session_sizes import SessionSizes
 
 __all__ = [
-    GdsSessions,
-    SessionInfo,
-    DbmsConnectionInfo,
-    AuraAPICredentials,
-    SessionSizes,
+    "GdsSessions",
+    "SessionInfo",
+    "DbmsConnectionInfo",
+    "AuraAPICredentials",
+    "SessionSizes",
 ]
-
-
