63eeb5aee9c68e0d98fe0f91f47dbd05c6489227
Florentin Dörre <florentin.dorre@neo4j.com>
2024-02-14 16:35:19 +0100

    Expose user-facing classes on session level
    Shorten previous nested imports

---
+++ graphdatascience/gds_session/__init__.py
@@ -0,0 +1,13 @@
+from .gds_sessions import GdsSessions, SessionInfo, AuraAPICredentials
+from .dbms_connection_info import DbmsConnectionInfo
+from .session_sizes import SessionSizes
+
+__all__ = [
+    GdsSessions,
+    SessionInfo,
+    DbmsConnectionInfo,
+    AuraAPICredentials,
+    SessionSizes,
+]
+
+
