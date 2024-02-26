5769ece7d086e180f1ee376767e2a509e6d8835d
Florentin Dörre <florentin.dorre@neo4j.com>
2024-02-26 16:43:36 +0100

    Allow running session notebooks
    Seperate command, as they will start a GDS session instance as part of the notebook.
    Other notebooks take a neo4j instance with GDS installed as a setup.

--- scripts/run_notebooks.py
+++ scripts/run_notebooks.py
@@ -3,6 +3,7 @@
 from datetime import datetime
 from pathlib import Path
 from typing import Any
+import sys
 
 import nbformat
 from nbclient.exceptions import CellExecutionError
@@ -31,31 +32,40 @@ class GdsExecutePreprocessor(ExecutePreprocessor):
                 raise e
 
 
-examples_path = Path("examples")
-notebook_files = [f for f in examples_path.iterdir() if f.is_file()]
-ep = GdsExecutePreprocessor(kernel_name="python3")
+def main(run_session_nbs: bool) -> None:
+    examples_path = Path("examples")
+    
+    notebook_files = [f for f in examples_path.iterdir()
+                      if f.is_file() and ("session" in f.name) == run_session_nbs
+                      ]
 
-for notebook_filename in notebook_files:
-    now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    print(f"{now}: Executing notebook {notebook_filename}", flush=True)
-    if "gds-sessions" in notebook_filename.name:
-        print("Skipping test of Aura only notebook")
-        continue
+    ep = GdsExecutePreprocessor(kernel_name="python3")
 
-    with open(notebook_filename) as f:
-        nb = nbformat.read(f, as_version=4)  # type: ignore
+    for notebook_filename in notebook_files:
+        now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        print(f"{now}: Executing notebook {notebook_filename}", flush=True)
 
-        # Check if the GDS version matches
-        # ep.execute_cell
-        verify_version_cell_index = [
-            idx for idx, cell in enumerate(nb["cells"]) if VERSION_CELL_TAG in cell["metadata"].get("tags", [])
-        ]
-        if not verify_version_cell_index or len(verify_version_cell_index) > 1:
-            raise ValueError(
-                f"Notebook {notebook_filename} does not have a cell tagged with '{VERSION_CELL_TAG}'."
-                "Required to run the notebook only against compatible versions."
-            )
-        ep.init_notebook(verify_version_cell_index[0])
-        # run the notebook
-        ep.preprocess(nb)
-    print(f"Finished executing notebook {notebook_filename}")
+        with open(notebook_filename) as f:
+            nb = nbformat.read(f, as_version=4)  # type: ignore
+
+            # Check if the GDS version matches
+            # ep.execute_cell
+            verify_version_cell_index = [
+                idx for idx, cell in enumerate(nb["cells"]) if VERSION_CELL_TAG in cell["metadata"].get("tags", [])
+            ]
+            if not verify_version_cell_index or len(verify_version_cell_index) > 1:
+                raise ValueError(
+                    f"Notebook {notebook_filename} does not have a cell tagged with '{VERSION_CELL_TAG}'."
+                    "Required to run the notebook only against compatible versions."
+                )
+            ep.init_notebook(verify_version_cell_index[0])
+            # run the notebook
+            ep.preprocess(nb)
+        print(f"Finished executing notebook {notebook_filename}")
+
+ 
+if __name__ == "__main__":
+    notebook_filter = sys.argv[1] if len(sys.argv) >= 2 else ''
+    only_session_nbs = True if notebook_filter == 'session' else False
+
+    main(only_session_nbs)
