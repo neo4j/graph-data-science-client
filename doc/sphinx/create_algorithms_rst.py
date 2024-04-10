808be37e2c88d11c0dad635521d6d7bf0c92ca4c
Nicola Vitucci <nicola.vitucci@neo4j.com>
2024-04-10 16:27:35 +0100

    Reformat script

--- doc/sphinx/create_algorithms_rst.py
+++ doc/sphinx/create_algorithms_rst.py
@@ -5,7 +5,9 @@ from textwrap import dedent
 with open("algorithms.json") as f, open("source/algorithms.rst", "w") as fw:
     functions = json.load(f)
 
-    fw.write(dedent("""\
+    fw.write(
+        dedent(
+            """\
         ..
             DO NOT EDIT - File generated automatically
 
@@ -14,18 +16,24 @@ with open("algorithms.json") as f, open("source/algorithms.rst", "w") as fw:
         Listing of all algorithm procedures in the Neo4j Graph Data Science Python Client API.
         These all assume that an object of :class:`.GraphDataScience` is available as `gds`.
 
-        """))
-    
+        """
+        )
+    )
+
     for function in functions:
-        name, sig, ret_type = function["function"]["name"], function["function"]["signature"], function["function"]["return_type"]
+        name, sig, ret_type = (
+            function["function"]["name"],
+            function["function"]["signature"],
+            function["function"]["return_type"],
+        )
         fw.write(f".. py:function:: {name}({sig}) -> {ret_type}\n\n")
 
         if "description" in function:
-          description = function["description"].strip()
-          for desc in description.split("\n"):
-              fw.write(f"    {desc}\n")
+            description = function["description"].strip()
+            for desc in description.split("\n"):
+                fw.write(f"    {desc}\n")
 
-          fw.write("\n")
+            fw.write("\n")
 
         if "deprecated" in function:
             version, message = function["deprecated"]["version"], function["deprecated"]["message"]
