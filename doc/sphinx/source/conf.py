d0feba2258312461b1fab07b53162abdafb22c8c
Mats Rydberg <mats@neo4j.org>
2024-04-18 12:44:48 +0200

    Update version and copyright

--- doc/sphinx/source/conf.py
+++ doc/sphinx/source/conf.py
@@ -9,9 +9,9 @@ import sys
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = "Neo4j Graph Data Science Python Client"
-copyright = "2023, Neo4j"
+copyright = "2024, Neo4j"
 author = "Neo4j"
-version = "1.9"
+version = "1.11"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
