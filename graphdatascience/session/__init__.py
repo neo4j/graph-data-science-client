66f168400f615662631c475a6aa28081d0853339
Mats Rydberg <mats@neo4j.org>
2024-06-18 10:03:25 +0200

    Remove obsolete schema

--- graphdatascience/session/__init__.py
+++ graphdatascience/session/__init__.py
@@ -1,7 +1,6 @@
 from .algorithm_category import AlgorithmCategory
 from .dbms_connection_info import DbmsConnectionInfo
 from .gds_sessions import AuraAPICredentials, GdsSessions
-from .schema import GdsPropertyTypes
 from .session_info import SessionInfo
 from .session_sizes import SessionMemory
 
@@ -11,6 +10,5 @@ __all__ = [
     "DbmsConnectionInfo",
     "AuraAPICredentials",
     "SessionMemory",
-    "GdsPropertyTypes",
     "AlgorithmCategory",
 ]
