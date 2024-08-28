1d03068c505094de4bc49818e02e3e395f9b3f74
Max Kießling <max.kiessling@neo4j.com>
2024-08-28 10:33:50 +0200

    Print files with differences in scripts/nb2doc/check.sh

--- scripts/nb2doc/check.sh
+++ scripts/nb2doc/check.sh
@@ -3,7 +3,6 @@
 set -o errexit
 set -o nounset
 set -o pipefail
-set -o xtrace
 
 ${PWD}/scripts/nb2doc/convert.sh
 
@@ -13,6 +12,9 @@ export GIT_DIR=${PWD}/.git
 
 if ! git diff --quiet ${PWD}/doc/modules/ROOT/pages/tutorials/
 then
+  diff_files=`git diff ${PWD}/doc/modules/ROOT/pages/tutorials/ | grep "diff --git" | cut -d " " -f3`
+  echo "The following notebooks need to be updated in the docs"
+  echo "${diff_files}"
   echo "Please run /scripts/nb2doc/convert.sh to update docs"
   exit 1
 fi
