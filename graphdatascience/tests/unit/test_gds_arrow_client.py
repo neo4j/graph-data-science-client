4b08a9bf6be6011567729c309a36ddfe2b0760f5
Max Kießling <max.kiessling@neo4j.com>
2024-09-04 15:42:07 +0200

    Fix code style

--- graphdatascience/tests/unit/test_gds_arrow_client.py
+++ graphdatascience/tests/unit/test_gds_arrow_client.py
@@ -31,15 +31,24 @@ def test_auth_middleware_bad_headers() -> None:
     with pytest.raises(ValueError, match="Incompatible header value received from server: `12342`"):
         middleware.received_headers({"authorization": [12342]})
 
+
 def test_handle_flight_error():
     with pytest.raises(
-            flight.FlightServerError, match="FlightServerError: UNKNOWN: Graph with name `people-and-fruits` does not exist on database `neo4j`. It might exist on another database."
+        flight.FlightServerError,
+        match="FlightServerError: UNKNOWN: Graph with name `people-and-fruits` does not exist on database `neo4j`. It might exist on another database.",
     ):
-        GdsArrowClient.handle_flight_error(flight.FlightServerError(
-            "FlightServerError: Flight RPC failed with message: org.apache.arrow.flight.FlightRuntimeException: UNKNOWN: Graph with name `people-and-fruits` does not exist on database `neo4j`. It might exist on another database.. gRPC client debug context: UNKNOWN:Error received from peer ipv4:35.241.177.75:8491 {created_time:\"2024-08-29T15:59:03.828903999+02:00\", grpc_status:2, grpc_message:\"org.apache.arrow.flight.FlightRuntimeException: UNKNOWN: Graph with name `people-and-fruits` does not exist on database `neo4j`. It might exist on another database.\"}. Client context: IOError: Server never sent a data message. Detail: Internal"))
+        GdsArrowClient.handle_flight_error(
+            flight.FlightServerError(
+                'FlightServerError: Flight RPC failed with message: org.apache.arrow.flight.FlightRuntimeException: UNKNOWN: Graph with name `people-and-fruits` does not exist on database `neo4j`. It might exist on another database.. gRPC client debug context: UNKNOWN:Error received from peer ipv4:35.241.177.75:8491 {created_time:"2024-08-29T15:59:03.828903999+02:00", grpc_status:2, grpc_message:"org.apache.arrow.flight.FlightRuntimeException: UNKNOWN: Graph with name `people-and-fruits` does not exist on database `neo4j`. It might exist on another database."}. Client context: IOError: Server never sent a data message. Detail: Internal'
+            )
+        )
 
     with pytest.raises(
-            flight.FlightServerError, match=re.escape("FlightServerError: UNKNOWN: Unexpected configuration key(s): [undirectedRelationshipTypes]")
+        flight.FlightServerError,
+        match=re.escape("FlightServerError: UNKNOWN: Unexpected configuration key(s): [undirectedRelationshipTypes]"),
     ):
-        GdsArrowClient.handle_flight_error(flight.FlightServerError(
-            "FlightServerError: Flight returned internal error, with message: org.apache.arrow.flight.FlightRuntimeException: UNKNOWN: Unexpected configuration key(s): [undirectedRelationshipTypes]"))
+        GdsArrowClient.handle_flight_error(
+            flight.FlightServerError(
+                "FlightServerError: Flight returned internal error, with message: org.apache.arrow.flight.FlightRuntimeException: UNKNOWN: Unexpected configuration key(s): [undirectedRelationshipTypes]"
+            )
+        )
