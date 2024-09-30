b9028de2804b97f4632586973ff6e52015bc692f
Mats Rydberg <mats@neo4j.org>
2024-09-30 16:32:59 +0200

    Squeeze results to enable string indexing

--- graphdatascience/tests/integration/test_remote_graph_ops.py
+++ graphdatascience/tests/integration/test_remote_graph_ops.py
@@ -60,10 +60,10 @@ def test_remote_projection_and_writeback_custom_database_name(gds_with_cloud_set
     write_result = gds_with_cloud_setup.wcc.write(G, writeProperty="wcc")
 
     assert write_result["nodePropertiesWritten"] == 2
-    db_state = gds_with_cloud_setup.run_cypher("MATCH (n WHERE n.wcc IS NOT NULL) RETURN count(*) AS c")
+    db_state = gds_with_cloud_setup.run_cypher("MATCH (n WHERE n.wcc IS NOT NULL) RETURN count(*) AS c").squeeze()
     assert db_state["c"] == 2
     gds_with_cloud_setup.set_database("neo4j")
-    other_db_state = gds_with_cloud_setup.run_cypher("MATCH (n WHERE n.wcc IS NOT NULL) RETURN count(*) AS c")
+    other_db_state = gds_with_cloud_setup.run_cypher("MATCH (n WHERE n.wcc IS NOT NULL) RETURN count(*) AS c").squeeze()
     assert other_db_state["c"] == 0
 
 
