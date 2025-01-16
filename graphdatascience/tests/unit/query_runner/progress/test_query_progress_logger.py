45a24bc511f9207dac8762b9dd9f3eff2f7a9453
Florentin Dörre <florentin.dorre@neo4j.com>
2025-01-16 13:26:16 +0100

    Fix tests

--- graphdatascience/tests/unit/query_runner/progress/test_query_progress_logger.py
+++ graphdatascience/tests/unit/query_runner/progress/test_query_progress_logger.py
@@ -12,10 +12,10 @@ from graphdatascience.tests.unit.conftest import CollectingQueryRunner
 
 def test_call_through_functions() -> None:
     def fake_run_cypher(query: str, database: Optional[str] = None) -> DataFrame:
-        assert query == "CALL gds.listProgress('foo') YIELD taskName, progress RETURN taskName, progress LIMIT 1"
+        assert query == "CALL gds.listProgress('foo') YIELD taskName, progress, status RETURN taskName, progress, status LIMIT 1"
         assert database == "database"
 
-        return DataFrame([{"progress": "n/a", "taskName": "Test task"}])
+        return DataFrame([{"progress": "n/a", "taskName": "Test task", "status": "RUNNING"}])
 
     def fake_query() -> DataFrame:
         time.sleep(1)
@@ -43,10 +43,10 @@ def test_skips_progress_logging_for_old_server_version() -> None:
 
 def test_uses_beta_endpoint() -> None:
     def fake_run_cypher(query: str, database: Optional[str] = None) -> DataFrame:
-        assert query == "CALL gds.beta.listProgress('foo') YIELD taskName, progress RETURN taskName, progress LIMIT 1"
+        assert query == "CALL gds.beta.listProgress('foo') YIELD taskName, progress, status RETURN taskName, progress, status LIMIT 1"
         assert database == "database"
 
-        return DataFrame([{"progress": "n/a", "taskName": "Test task"}])
+        return DataFrame([{"progress": "n/a", "taskName": "Test task", "status": "RUNNING"}])
 
     def fake_query() -> DataFrame:
         time.sleep(1)
@@ -72,7 +72,7 @@ def test_uses_query_provider() -> None:
 
 def test_uses_static_store() -> None:
     def fake_run_cypher(query: str, database: Optional[str] = None) -> DataFrame:
-        return DataFrame([{"progress": "n/a", "taskName": "Test task"}])
+        return DataFrame([{"progress": "n/a", "taskName": "Test task", "status": "RUNNING"}])
 
     qpl = QueryProgressLogger(fake_run_cypher, lambda: ServerVersion(3, 0, 0))
     StaticProgressStore.register_task_with_unknown_volume("test-job", "Test task")
