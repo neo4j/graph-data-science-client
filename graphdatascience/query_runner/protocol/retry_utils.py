9fca5074b00bc19a2e7e6606fdbbc8e3a5cba3de
Florentin Dörre <florentin.dorre@neo4j.com>
2025-01-29 11:26:55 +0100

    Remove unused code

--- graphdatascience/query_runner/protocol/retry_utils.py
+++ graphdatascience/query_runner/protocol/retry_utils.py
@@ -1,31 +1,7 @@
 import logging
-import signal
 import typing
-from types import FrameType
-from typing import Optional
 
-from tenacity import RetryCallState, retry_base
-
-
-class retry_unless_signal(retry_base):
-    """Retries unless one of the given signals is raised."""
-
-    def __init__(self, signals: list[signal.Signals]) -> None:
-        self.signal_received = False
-
-        def receive_signal(sig: int, frame: Optional[FrameType]) -> None:
-            logging.debug(f"Received signal {sig}. Interrupting retry.")
-            self.signal_received = True
-
-        try:
-            for sig in signals:
-                signal.signal(sig, receive_signal)
-        except ValueError as e:
-            # signal.signal() can raise ValueError if this is not called in the main thread (such as when an algorithm is called in a ThreadPool)
-            logging.debug(f"Cannot set signal handler for retries {e}")
-
-    def __call__(self, retry_state: RetryCallState) -> bool:
-        return not self.signal_received
+from tenacity import RetryCallState
 
 
 def before_log(
