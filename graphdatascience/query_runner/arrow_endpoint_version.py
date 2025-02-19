634705912a0016978739cbea8758872095b9c061
Max Kießling <max.kiessling@neo4j.com>
2025-02-19 10:51:27 +0100

    Remove Alpha Arrow version support

--- graphdatascience/query_runner/arrow_endpoint_version.py
+++ graphdatascience/query_runner/arrow_endpoint_version.py
@@ -4,8 +4,8 @@ from enum import Enum
 
 
 class ArrowEndpointVersion(Enum):
-    ALPHA = ""
     V1 = "v1/"
+    V2 = "v2/"
 
     def version(self) -> str:
         return self._name_.lower()
@@ -15,16 +15,9 @@ class ArrowEndpointVersion(Enum):
 
     @staticmethod
     def from_arrow_info(supported_arrow_versions: list[str]) -> ArrowEndpointVersion:
-        # Fallback for pre 2.6.0 servers that do not support versions
-        if len(supported_arrow_versions) == 0:
-            return ArrowEndpointVersion.ALPHA
-
         # If the server supports versioned endpoints, we try v1 first
         if ArrowEndpointVersion.V1.version() in supported_arrow_versions:
             return ArrowEndpointVersion.V1
-
-        if ArrowEndpointVersion.ALPHA.version() in supported_arrow_versions:
-            return ArrowEndpointVersion.ALPHA
 
         raise UnsupportedArrowEndpointVersion(supported_arrow_versions)
 
--- graphdatascience/tests/unit/test_arrow_runner.py
+++ graphdatascience/tests/unit/test_arrow_runner.py
@@ -6,11 +6,12 @@ from graphdatascience.query_runner.arrow_query_runner import ArrowQueryRunner
 from graphdatascience.server_version.server_version import ServerVersion
 
 from .conftest import CollectingQueryRunner
+from ...query_runner.arrow_endpoint_version import ArrowEndpointVersion
 
 
 @pytest.mark.parametrize("server_version", [ServerVersion(2, 6, 0)])
 def test_create(runner: CollectingQueryRunner) -> None:
-    arrow_info = ArrowInfo(listenAddress="localhost:1234", enabled=True, running=True, versions=[])
+    arrow_info = ArrowInfo(listenAddress="localhost:1234", enabled=True, running=True, versions=[ArrowEndpointVersion.V1.version()])
     arrow_runner = ArrowQueryRunner.create(runner, arrow_info)
 
     assert isinstance(arrow_runner, ArrowQueryRunner)
@@ -29,7 +30,7 @@ def test_return_fallback_when_arrow_is_not_enabled(runner: CollectingQueryRunner
 
 @pytest.mark.parametrize("server_version", [ServerVersion(2, 6, 0)])
 def test_create_with_provided_connection(runner: CollectingQueryRunner) -> None:
-    arrow_info = ArrowInfo(listenAddress="localhost:1234", enabled=True, running=True, versions=[])
+    arrow_info = ArrowInfo(listenAddress="localhost:1234", enabled=True, running=True, versions=[ArrowEndpointVersion.V1.version()])
 
     arrow_runner = ArrowQueryRunner.create(runner, arrow_info, connection_string_override="localhost:4321")
 
