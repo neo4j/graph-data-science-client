98aa1dc197bee139dfb8547fe501d6b629021af7
Florentin Dörre <florentin.dorre@neo4j.com>
2025-03-19 16:56:53 +0100

    Log on retries

--- graphdatascience/retry_utils/retry_utils.py
+++ graphdatascience/retry_utils/retry_utils.py
@@ -11,9 +11,10 @@ def before_log(
     sec_format: str = "%0.3f",
 ) -> typing.Callable[[RetryCallState], None]:
     def log_it(retry_state: RetryCallState) -> None:
-        logger.log(
-            log_level,
-            f"Retry of '{fn_name}', " f"attempt: {retry_state.attempt_number}",
-        )
+        if retry_state.attempt_number > 1:  # only log on actual retry
+            logger.log(
+                log_level,
+                f"Retry of '{fn_name}', " f"attempt: {retry_state.attempt_number}",
+            )
 
     return log_it
