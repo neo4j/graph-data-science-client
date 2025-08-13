2948d96286fca7d11af434d3be3e7cfd4cca024b
Florentin Dörre <florentin.dorre@neo4j.com>
2025-08-13 11:58:32 +0200

    Add harmonic centrality

---
+++ graphdatascience/procedure_surface/arrow/closeness_harmonic_arrow_endpoints.py
@@ -0,0 +1,142 @@
+from typing import Any, List, Optional, Union
+
+from pandas import DataFrame
+
+from ...arrow_client.authenticated_flight_client import AuthenticatedArrowClient
+from ...arrow_client.v2.write_back_client import WriteBackClient
+from ...graph.graph_object import Graph
+from ..api.closeness_harmonic_endpoints import (
+    ClosenessHarmonicEndpoints,
+    ClosenessHarmonicMutateResult,
+    ClosenessHarmonicStatsResult,
+    ClosenessHarmonicWriteResult,
+)
+from ..api.estimation_result import EstimationResult
+from .node_property_endpoints import NodePropertyEndpoints
+
+
+class ClosenessHarmonicArrowEndpoints(ClosenessHarmonicEndpoints):
+    def __init__(self, arrow_client: AuthenticatedArrowClient, write_back_client: Optional[WriteBackClient] = None):
+        self._node_property_endpoints = NodePropertyEndpoints(arrow_client, write_back_client)
+
+    def mutate(
+        self,
+        G: Graph,
+        mutate_property: str,
+        relationship_types: Optional[List[str]] = None,
+        node_labels: Optional[List[str]] = None,
+        sudo: Optional[bool] = None,
+        log_progress: Optional[bool] = None,
+        username: Optional[str] = None,
+        concurrency: Optional[Any] = None,
+        job_id: Optional[Any] = None,
+    ) -> ClosenessHarmonicMutateResult:
+        config = self._node_property_endpoints.create_base_config(
+            G,
+            concurrency=concurrency,
+            job_id=job_id,
+            log_progress=log_progress,
+            node_labels=node_labels,
+            relationship_types=relationship_types,
+            sudo=sudo,
+            username=username,
+        )
+
+        result = self._node_property_endpoints.run_job_and_mutate("v2/centrality.harmonic", G, config, mutate_property)
+
+        return ClosenessHarmonicMutateResult(**result)
+
+    def stats(
+        self,
+        G: Graph,
+        relationship_types: Optional[List[str]] = None,
+        node_labels: Optional[List[str]] = None,
+        sudo: Optional[bool] = None,
+        log_progress: Optional[bool] = None,
+        username: Optional[str] = None,
+        concurrency: Optional[Any] = None,
+        job_id: Optional[Any] = None,
+    ) -> ClosenessHarmonicStatsResult:
+        config = self._node_property_endpoints.create_base_config(
+            G,
+            concurrency=concurrency,
+            job_id=job_id,
+            log_progress=log_progress,
+            node_labels=node_labels,
+            relationship_types=relationship_types,
+            sudo=sudo,
+            username=username,
+        )
+
+        computation_result = self._node_property_endpoints.run_job_and_get_summary("v2/centrality.harmonic", G, config)
+
+        return ClosenessHarmonicStatsResult(**computation_result)
+
+    def stream(
+        self,
+        G: Graph,
+        relationship_types: Optional[List[str]] = None,
+        node_labels: Optional[List[str]] = None,
+        sudo: Optional[bool] = None,
+        log_progress: Optional[bool] = None,
+        username: Optional[str] = None,
+        concurrency: Optional[Any] = None,
+        job_id: Optional[Any] = None,
+    ) -> DataFrame:
+        config = self._node_property_endpoints.create_base_config(
+            G,
+            concurrency=concurrency,
+            job_id=job_id,
+            log_progress=log_progress,
+            node_labels=node_labels,
+            relationship_types=relationship_types,
+            sudo=sudo,
+            username=username,
+        )
+
+        return self._node_property_endpoints.run_job_and_stream("v2/centrality.harmonic", G, config)
+
+    def write(
+        self,
+        G: Graph,
+        write_property: str,
+        relationship_types: Optional[List[str]] = None,
+        node_labels: Optional[List[str]] = None,
+        sudo: Optional[bool] = None,
+        log_progress: Optional[bool] = None,
+        username: Optional[str] = None,
+        concurrency: Optional[Any] = None,
+        job_id: Optional[Any] = None,
+        write_concurrency: Optional[Any] = None,
+    ) -> ClosenessHarmonicWriteResult:
+        config = self._node_property_endpoints.create_base_config(
+            G,
+            concurrency=concurrency,
+            job_id=job_id,
+            log_progress=log_progress,
+            node_labels=node_labels,
+            relationship_types=relationship_types,
+            sudo=sudo,
+            username=username,
+        )
+
+        result = self._node_property_endpoints.run_job_and_write(
+            "v2/centrality.harmonic", G, config, write_concurrency, concurrency
+        )
+
+        return ClosenessHarmonicWriteResult(**result)
+
+    def estimate(
+        self,
+        G: Union[Graph, dict[str, Any]],
+        relationship_types: Optional[List[str]] = None,
+        node_labels: Optional[List[str]] = None,
+        concurrency: Optional[Any] = None,
+    ) -> EstimationResult:
+        algo_config = self._node_property_endpoints.create_estimate_config(
+            relationship_types=relationship_types,
+            node_labels=node_labels,
+            concurrency=concurrency,
+        )
+
+        return self._node_property_endpoints.estimate("v2/centrality.harmonic.estimate", G, algo_config)
---
+++ graphdatascience/procedure_surface/cypher/closeness_harmonic_cypher_endpoints.py
@@ -0,0 +1,169 @@
+from typing import Any, List, Optional, Union
+
+from pandas import DataFrame
+
+from graphdatascience.procedure_surface.cypher.estimation_utils import estimate_algorithm
+
+from ...call_parameters import CallParameters
+from ...graph.graph_object import Graph
+from ...query_runner.query_runner import QueryRunner
+from ..api.closeness_harmonic_endpoints import (
+    ClosenessHarmonicEndpoints,
+    ClosenessHarmonicMutateResult,
+    ClosenessHarmonicStatsResult,
+    ClosenessHarmonicWriteResult,
+)
+from ..api.estimation_result import EstimationResult
+from ..utils.config_converter import ConfigConverter
+
+
+class ClosenessHarmonicCypherEndpoints(ClosenessHarmonicEndpoints):
+    """Cypher-based implementation of Harmonic Closeness Centrality algorithm endpoints."""
+
+    def __init__(self, query_runner: QueryRunner):
+        self._query_runner = query_runner
+
+    def mutate(
+        self,
+        G: Graph,
+        mutate_property: str,
+        relationship_types: Optional[List[str]] = None,
+        node_labels: Optional[List[str]] = None,
+        sudo: Optional[bool] = None,
+        log_progress: Optional[bool] = None,
+        username: Optional[str] = None,
+        concurrency: Optional[Any] = None,
+        job_id: Optional[Any] = None,
+    ) -> ClosenessHarmonicMutateResult:
+        config = ConfigConverter.convert_to_gds_config(
+            mutateProperty=mutate_property,
+            relationshipTypes=relationship_types,
+            nodeLabels=node_labels,
+            sudo=sudo,
+            logProgress=log_progress,
+            username=username,
+            concurrency=concurrency,
+            jobId=job_id,
+        )
+
+        params = CallParameters(
+            graph_name=G.name(),
+            config=config,
+        )
+        params.ensure_job_id_in_config()
+
+        result = self._query_runner.call_procedure(endpoint="gds.closeness.harmonic.mutate", params=params).squeeze()
+        return ClosenessHarmonicMutateResult(**result.to_dict())
+
+    def stats(
+        self,
+        G: Graph,
+        relationship_types: Optional[List[str]] = None,
+        node_labels: Optional[List[str]] = None,
+        sudo: Optional[bool] = None,
+        log_progress: Optional[bool] = None,
+        username: Optional[str] = None,
+        concurrency: Optional[Any] = None,
+        job_id: Optional[Any] = None,
+    ) -> ClosenessHarmonicStatsResult:
+        config = ConfigConverter.convert_to_gds_config(
+            relationshipTypes=relationship_types,
+            nodeLabels=node_labels,
+            sudo=sudo,
+            logProgress=log_progress,
+            username=username,
+            concurrency=concurrency,
+            jobId=job_id,
+        )
+
+        params = CallParameters(
+            graph_name=G.name(),
+            config=config,
+        )
+        params.ensure_job_id_in_config()
+
+        result = self._query_runner.call_procedure(endpoint="gds.closeness.harmonic.stats", params=params).squeeze()
+        return ClosenessHarmonicStatsResult(**result.to_dict())
+
+    def stream(
+        self,
+        G: Graph,
+        relationship_types: Optional[List[str]] = None,
+        node_labels: Optional[List[str]] = None,
+        sudo: Optional[bool] = None,
+        log_progress: Optional[bool] = None,
+        username: Optional[str] = None,
+        concurrency: Optional[Any] = None,
+        job_id: Optional[Any] = None,
+    ) -> DataFrame:
+        config = ConfigConverter.convert_to_gds_config(
+            relationshipTypes=relationship_types,
+            nodeLabels=node_labels,
+            sudo=sudo,
+            logProgress=log_progress,
+            username=username,
+            concurrency=concurrency,
+            jobId=job_id,
+        )
+
+        params = CallParameters(
+            graph_name=G.name(),
+            config=config,
+        )
+        params.ensure_job_id_in_config()
+
+        return self._query_runner.call_procedure(endpoint="gds.closeness.harmonic.stream", params=params)
+
+    def write(
+        self,
+        G: Graph,
+        write_property: str,
+        relationship_types: Optional[List[str]] = None,
+        node_labels: Optional[List[str]] = None,
+        sudo: Optional[bool] = None,
+        log_progress: Optional[bool] = None,
+        username: Optional[str] = None,
+        concurrency: Optional[Any] = None,
+        job_id: Optional[Any] = None,
+        write_concurrency: Optional[Any] = None,
+    ) -> ClosenessHarmonicWriteResult:
+        config = ConfigConverter.convert_to_gds_config(
+            writeProperty=write_property,
+            relationshipTypes=relationship_types,
+            nodeLabels=node_labels,
+            sudo=sudo,
+            logProgress=log_progress,
+            username=username,
+            concurrency=concurrency,
+            jobId=job_id,
+            writeConcurrency=write_concurrency,
+        )
+
+        params = CallParameters(
+            graph_name=G.name(),
+            config=config,
+        )
+        params.ensure_job_id_in_config()
+
+        result = self._query_runner.call_procedure(endpoint="gds.closeness.harmonic.write", params=params).squeeze()
+        return ClosenessHarmonicWriteResult(**result.to_dict())
+
+    def estimate(
+        self,
+        G: Union[Graph, dict[str, Any]],
+        relationship_types: Optional[List[str]] = None,
+        node_labels: Optional[List[str]] = None,
+        concurrency: Optional[Any] = None,
+    ) -> EstimationResult:
+        algo_config = ConfigConverter.convert_to_gds_config(
+            relationshipTypes=relationship_types,
+            nodeLabels=node_labels,
+            concurrency=concurrency,
+        )
+
+        return estimate_algorithm(
+            endpoint="gds.closeness.harmonic.stats.estimate",
+            query_runner=self._query_runner,
+            G=G,
+            algo_config=algo_config,
+        )
