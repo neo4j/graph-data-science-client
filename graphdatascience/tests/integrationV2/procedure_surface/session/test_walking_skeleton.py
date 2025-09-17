e3a56cb49a1c41036462abe2f1b386ee8eb1a582
Mats Rydberg <mats@neo4j.org>
2025-09-17 17:30:14 +0200

    Rename class to avoid pytest interpreting it as a test

--- graphdatascience/tests/integrationV2/procedure_surface/session/test_walking_skeleton.py
+++ graphdatascience/tests/integrationV2/procedure_surface/session/test_walking_skeleton.py
@@ -6,7 +6,8 @@ from graphdatascience import QueryRunner, ServerVersion
 from graphdatascience.arrow_client.authenticated_flight_client import AuthenticatedArrowClient
 from graphdatascience.session.aura_graph_data_science import AuraGraphDataScience
 from graphdatascience.session.session_v2_endpoints import SessionV2Endpoints
-from graphdatascience.tests.integrationV2.procedure_surface.arrow.graph_creation_helper import TestArrowGraph
+from graphdatascience.tests.integrationV2.procedure_surface.arrow.graph_creation_helper import TestArrowGraph, \
+    ArrowGraphForTests
 
 
 @pytest.fixture(scope="package")
@@ -35,7 +36,7 @@ def setup_db(db_query_runner: QueryRunner) -> Generator[None, None, None]:
 @pytest.mark.db_integration
 def test_walking_skeleton(gds: AuraGraphDataScience) -> None:
     project_result = gds.v2.graph.project("g", "MATCH (n)-->(m) RETURN gds.graph.project.remote(n, m)")
-    G = TestArrowGraph(project_result.graph_name)
+    G = ArrowGraphForTests(project_result.graph_name)
 
     wcc_mutate_result = gds.v2.wcc.mutate(G, mutate_property="wcc")
 
