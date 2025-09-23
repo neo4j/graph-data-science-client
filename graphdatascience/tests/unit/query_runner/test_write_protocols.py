f3807d1341e28caa2ea87d20499d221ba2484809
Florentin Dörre <florentin.dorre@neo4j.com>
2025-09-23 17:25:32 +0200

    Fix write-back progress bar for write-back to self-managed dbs

--- graphdatascience/tests/unit/query_runner/test_write_protocols.py
+++ graphdatascience/tests/unit/query_runner/test_write_protocols.py
@@ -34,6 +34,30 @@ def test_write_back_v3_progress_logging() -> None:
         assert any(["Write-Back (graph: myGraph): 100%|##########| 100.0/100" in line for line in bar_output])
 
 
+def test_write_back_v3_progress_logging_without_bar() -> None:
+    # for self-managed dbs the endpoint doesnt return the progress yet
+    with StringIO() as pbarOutputStream:
+        qr = CollectingQueryRunner(ServerVersion(0, 0, 0))
+        qr.add__mock_result("gds.arrow.write.v3", DataFrame([{"status": Status.COMPLETED.name}]))
+
+        wp = RemoteWriteBackV3(progress_bar_options={"file": pbarOutputStream, "mininterval": 0})
+
+        wp.run_write_back(
+            query_runner=qr,
+            parameters=CallParameters(graphName="myGraph", jobId="myJob"),
+            log_progress=True,
+            terminationFlag=TerminationFlagNoop(),
+            yields=None,
+        )
+
+        bar_output = pbarOutputStream.getvalue().split("\r")
+
+        assert any(
+            ["Write-Back (graph: myGraph):   0%|          | 0.0/100 [00:00<?, ?%/s]" in line for line in bar_output]
+        ), bar_output
+        assert any(["Write-Back (graph: myGraph): 100%|##########| 100.0/100" in line for line in bar_output])
+
+
 def test_write_back_v3_progress_logging_aborted() -> None:
     with StringIO() as pbarOutputStream:
         qr = CollectingQueryRunner(ServerVersion(0, 0, 0))
