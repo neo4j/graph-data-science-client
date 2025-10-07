a087981929ed099d5f1d0d017a8226c089ec9c97
Florentin Dörre <florentin.dorre@neo4j.com>
2025-10-07 17:41:15 +0200

    Fix imports

--- graphdatascience/procedure_surface/cypher/maxkcut_cypher_endpoints.py
+++ graphdatascience/procedure_surface/cypher/maxkcut_cypher_endpoints.py
@@ -2,17 +2,16 @@ from typing import Any, List, Optional, Union
 
 from pandas import DataFrame
 
+from graphdatascience.call_parameters import CallParameters
 from graphdatascience.procedure_surface.api.catalog.graph_api import GraphV2
 from graphdatascience.procedure_surface.api.community.maxkcut_endpoints import (
     MaxKCutEndpoints,
     MaxKCutMutateResult,
 )
+from graphdatascience.procedure_surface.api.estimation_result import EstimationResult
 from graphdatascience.procedure_surface.cypher.estimation_utils import estimate_algorithm
-
-from ...call_parameters import CallParameters
-from ...query_runner.query_runner import QueryRunner
-from ..api.estimation_result import EstimationResult
-from ..utils.config_converter import ConfigConverter
+from graphdatascience.procedure_surface.utils.config_converter import ConfigConverter
+from graphdatascience.query_runner.query_runner import QueryRunner
 
 
 class MaxKCutCypherEndpoints(MaxKCutEndpoints):
