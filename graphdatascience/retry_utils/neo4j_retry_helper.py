94bc1bd4d5050f9f6cc4c6134b3d4d868c475a64
Florentin Dörre <florentin.dorre@neo4j.com>
2025-10-10 14:28:24 +0200

    Test retryable

---
+++ graphdatascience/retry_utils/neo4j_retry_helper.py
@@ -0,0 +1,14 @@
+import neo4j
+
+
+def is_retryable_neo4j_exception(exception: BaseException) -> bool:
+    if neo4j.__version__.startswith("4."):
+        return False
+
+    if (
+        isinstance(exception, neo4j.exceptions.Neo4jError)
+        or isinstance(exception, neo4j.exceptions.DriverError)
+        and hasattr(exception, "is_retryable")
+    ):
+        return exception.is_retryable()
+    return False
