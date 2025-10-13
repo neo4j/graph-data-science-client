f946973012ed6a96b57d823fb323c90807cd292e
Florentin Dörre <florentin.dorre@neo4j.com>
2025-10-13 13:59:08 +0200

    Simplify isinstance check

--- graphdatascience/retry_utils/neo4j_retry_helper.py
+++ graphdatascience/retry_utils/neo4j_retry_helper.py
@@ -5,10 +5,8 @@ def is_retryable_neo4j_exception(exception: BaseException) -> bool:
     if neo4j.__version__.startswith("4."):
         return False
 
-    if (
-        isinstance(exception, neo4j.exceptions.Neo4jError)
-        or isinstance(exception, neo4j.exceptions.DriverError)
-        and hasattr(exception, "is_retryable")
+    if isinstance(exception, neo4j.exceptions.Neo4jError | neo4j.exceptions.DriverError) and hasattr(
+        exception, "is_retryable"
     ):
         return exception.is_retryable()
     return False
--- graphdatascience/tests/unit/retry_utils/test_neo4j_retry_helper.py
+++ graphdatascience/tests/unit/retry_utils/test_neo4j_retry_helper.py
@@ -6,7 +6,7 @@ from graphdatascience.server_version.server_version import ServerVersion
 
 
 @pytest.mark.compatible_with_db_driver(max_exclusive=ServerVersion(5, 0, 0))
-def test_returns_false_for_neo4j_version_4():
+def test_returns_false_for_neo4j_version_4() -> None:
     # Any exception should return False for version 4.x
     exception = Exception("test exception")
     assert is_retryable_neo4j_exception(exception) is False
@@ -21,7 +21,7 @@ def test_returns_false_for_neo4j_version_4():
 
 
 @pytest.mark.compatible_with_db_driver(min_inclusive=ServerVersion(5, 0, 0))
-def test_is_retryable_neo4j_exception():
+def test_is_retryable_neo4j_exception() -> None:
     assert is_retryable_neo4j_exception(neo4j.exceptions.TransientError("test"))
     assert is_retryable_neo4j_exception(neo4j.exceptions.SessionExpired("test"))
     assert is_retryable_neo4j_exception(neo4j.exceptions.ReadServiceUnavailable("test"))
