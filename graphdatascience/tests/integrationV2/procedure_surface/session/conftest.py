8640147045f6bd3b8ef1c82411923d455ce06021
Florentin Dörre <florentin.dorre@neo4j.com>
2025-10-16 15:22:55 +0200

    Pass connection info instead of docker container in conftest
    easier to also replace with an address of your own

--- graphdatascience/tests/integrationV2/procedure_surface/session/conftest.py
+++ graphdatascience/tests/integrationV2/procedure_surface/session/conftest.py
@@ -2,12 +2,13 @@ from pathlib import Path
 from typing import Generator
 
 import pytest
-from testcontainers.core.container import DockerContainer
 from testcontainers.core.network import Network
 
 from graphdatascience import QueryRunner
 from graphdatascience.arrow_client.authenticated_flight_client import AuthenticatedArrowClient
+from graphdatascience.session.dbms_connection_info import DbmsConnectionInfo
 from graphdatascience.tests.integrationV2.procedure_surface.conftest import (
+    GdsSessionConnectionInfo,
     create_arrow_client,
     create_db_query_runner,
     start_database,
@@ -16,20 +17,22 @@ from graphdatascience.tests.integrationV2.procedure_surface.conftest import (
 
 
 @pytest.fixture(scope="package")
-def session_url(network: Network, password_dir: Path, logs_dir: Path, inside_ci: bool) -> Generator[str, None, None]:
+def session_connection(
+    network: Network, password_dir: Path, logs_dir: Path, inside_ci: bool
+) -> Generator[GdsSessionConnectionInfo, None, None]:
     yield from start_session(inside_ci, logs_dir, network, password_dir)
 
 
 @pytest.fixture(scope="package")
-def arrow_client(session_url: str) -> AuthenticatedArrowClient:
-    return create_arrow_client(session_url)
+def arrow_client(session_connection: GdsSessionConnectionInfo) -> AuthenticatedArrowClient:
+    return create_arrow_client(session_connection)
 
 
 @pytest.fixture(scope="package")
-def neo4j_container(network: Network, logs_dir: Path, inside_ci: bool) -> Generator[DockerContainer, None, None]:
+def neo4j_connection(network: Network, logs_dir: Path, inside_ci: bool) -> Generator[DbmsConnectionInfo, None, None]:
     yield from start_database(inside_ci, logs_dir, network)
 
 
 @pytest.fixture(scope="package")
-def db_query_runner(neo4j_container: DockerContainer) -> Generator[QueryRunner, None, None]:
-    yield from create_db_query_runner(neo4j_container)
+def db_query_runner(neo4j_connection: DbmsConnectionInfo) -> Generator[QueryRunner, None, None]:
+    yield from create_db_query_runner(neo4j_connection)
