51ee48423a39f18502066b7d4bd5893b8c64a85e
Florentin Dörre <florentin.dorre@neo4j.com>
2025-11-10 19:54:08 +0100

    Fix style + default for scaleProperties

--- graphdatascience/procedure_surface/api/catalog/scaler_config.py
+++ graphdatascience/procedure_surface/api/catalog/scaler_config.py
@@ -6,5 +6,6 @@ class ScalerConfig(BaseModel):
         description="The type of scaler to use. Can be 'MinMax', 'Mean', 'Max', 'Log', 'StdScore', 'Center'."
     )
     offset: int | float | None = Field(
-        description="The offset to add to the property values before applying the log transformation. Only used when type is 'Log'."
+        default=None,
+        description="The offset to add to the property values before applying the log transformation. Only used when type is 'Log'.",
     )
