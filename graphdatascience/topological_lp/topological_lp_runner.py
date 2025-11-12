e1075e1a1acadb599012fae5722f49859fa954b2
Florentin Dörre <florentin.dorre@neo4j.com>
2025-11-12 15:09:46 +0100

    Support promoted adamicAdar

---
+++ graphdatascience/topological_lp/topological_lp_runner.py
@@ -0,0 +1,28 @@
+from typing import Any
+
+from graphdatascience.query_runner.query_mode import QueryMode
+from graphdatascience.server_version.compatible_with import compatible_with
+from graphdatascience.server_version.server_version import ServerVersion
+
+from ..error.cypher_warning_handler import (
+    filter_id_func_deprecation_warning,
+)
+from ..error.illegal_attr_checker import IllegalAttrChecker
+from ..error.uncallable_namespace import UncallableNamespace
+
+
+class TopologicalLPRunner(UncallableNamespace, IllegalAttrChecker):
+    @filter_id_func_deprecation_warning()
+    def _run_standard_function(self, node1: int, node2: int, config: dict[str, Any]) -> float:
+        query = f"""
+        MATCH (n1) WHERE id(n1) = {node1}
+        MATCH (n2) WHERE id(n2) = {node2}
+        RETURN {self._namespace}(n1, n2, $config) AS score
+        """
+        params = {"config": config}
+        return self._query_runner.run_retryable_cypher(query, params, mode=QueryMode.READ)["score"].squeeze()  # type: ignore
+
+    @compatible_with("adamicAdar", min_inclusive=ServerVersion(2, 24, 0))
+    def adamicAdar(self, node1: int, node2: int, **config: Any) -> float:
+        self._namespace += ".adamicAdar"
+        return self._run_standard_function(node1, node2, config)
