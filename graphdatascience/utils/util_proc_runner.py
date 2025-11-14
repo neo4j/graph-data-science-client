cb245f592c2b6da1738f6a84cf4f06a5da25994d
Florentin Dörre <florentin.dorre@neo4j.com>
2025-11-14 13:07:16 +0100

    Map promoted oneHotEncoding

--- graphdatascience/utils/util_proc_runner.py
+++ graphdatascience/utils/util_proc_runner.py
@@ -1,5 +1,7 @@
 from typing import Any
 
+from graphdatascience.call_parameters import CallParameters
+
 from ..error.illegal_attr_checker import IllegalAttrChecker
 from ..error.uncallable_namespace import UncallableNamespace
 from ..utils.util_node_property_func_runner import NodePropertyFuncRunner
@@ -43,3 +45,25 @@ class UtilProcRunner(UncallableNamespace, IllegalAttrChecker):
     @property
     def nodeProperty(self) -> NodePropertyFuncRunner:
         return NodePropertyFuncRunner(self._query_runner, self._namespace + ".nodeProperty", self._server_version)
+
+    def oneHotEncoding(self, available_values: list[Any], selected_values: list[Any]) -> list[int]:
+        """
+        One hot encode a list of values.
+
+        Args:
+            available_values: The available values to encode.
+            selected_values: The values to encode.
+
+        Returns:
+            The one hot encoded values.
+        """
+        namespace = self._namespace + ".oneHotEncoding"
+
+        params = CallParameters(
+            available_values=available_values,
+            selected_values=selected_values,
+        )
+        query = f"RETURN {namespace}($available_values, $selected_values) AS encoded"
+        result = self._query_runner.run_cypher(query=query, params=params)
+
+        return result.iat[0, 0]  # type: ignore
