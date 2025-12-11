a128dccdaf2d56f2d014d74631c6684c63d5363b
Florentin Dörre <florentin.dorre@neo4j.com>
2025-12-11 17:22:53 +0100

    Fix test

--- tests/integration/test_db_environment_resolver.py
+++ tests/integration/test_db_environment_resolver.py
@@ -5,8 +5,8 @@ from graphdatascience.query_runner.neo4j_query_runner import Neo4jQueryRunner
 
 
 @pytest.mark.only_on_aura
-def test_hosted_in_aura_aura_dbms(aura_runner: Neo4jQueryRunner) -> None:
-    assert DbEnvironmentResolver.hosted_in_aura(aura_runner)
+def test_hosted_in_aura_aura_dbms(runner: Neo4jQueryRunner) -> None:
+    assert DbEnvironmentResolver.hosted_in_aura(runner)
 
 
 @pytest.mark.skip_on_aura
