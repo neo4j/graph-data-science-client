8fa5491ca45f9abd468624be3312cc01a7498e58
Max Kießling <max.kiessling@neo4j.com>
2026-01-07 14:29:53 +0100

    Introduce model catalog endpoints

---
+++ src/graphdatascience/procedure_surface/api/model/__init__.py
@@ -0,0 +1,16 @@
+
+from .model_catalog_endpoints import (
+    ModelCatalogEndpoints,
+    ModelDeleteResult,
+    ModelExistsResult,
+    ModelLoadResult,
+    ModelStoreResult,
+)
+
+__all__ = [
+    "ModelCatalogEndpoints",
+    "ModelDeleteResult",
+    "ModelExistsResult",
+    "ModelLoadResult",
+    "ModelStoreResult",
+]
