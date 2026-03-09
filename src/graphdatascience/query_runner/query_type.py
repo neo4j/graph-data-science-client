84974083417294a6ec474d502881905c7a401fbc
Florentin Dörre <florentin.dorre@neo4j.com>
2026-03-09 10:16:36 +0100

    Use Enum
    StrEnum was only added in py 3.11

--- src/graphdatascience/query_runner/query_type.py
+++ src/graphdatascience/query_runner/query_type.py
@@ -1,8 +1,8 @@
-from enum import StrEnum
+from enum import Enum
 
 
 # following https://neo4j.com/docs/operations-manual/current/monitoring/logging/#attach-metadata-tx
-class QueryType(StrEnum):
+class QueryType(Enum):
     SYSTEM = "system"  # a query automatically run by the app.
     USER_DIRECTED = "user-direct"  # a query the user directly submitted to/through the app.
     USER_ACTION = "user-action"  # a query resulting from an action the user performed.
