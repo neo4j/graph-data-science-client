a29a892c7740285e864c0bd3a371d46f4de729c0
Florentin Dörre <florentin.dorre@neo4j.com>
2026-03-30 13:43:17 +0200

    Format code

--- scripts/release_helper/pre_release.py
+++ scripts/release_helper/pre_release.py
@@ -105,7 +105,9 @@ def main() -> None:
 
     verify_installation_docs(repo_dir, version, min_server_version)
 
-    print(f"All checks passed. Now find the latest prepare build to release from at https://live.neo4j-build.io/buildConfiguration/GraphAnalytics_Prepare_gdsclient")
+    print(
+        "All checks passed. Now find the latest prepare build to release from at https://live.neo4j-build.io/buildConfiguration/GraphAnalytics_Prepare_gdsclient"
+    )
 
 
 if __name__ == "__main__":
