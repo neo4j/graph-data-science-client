0fa2e280172f3ccf22354d999f5b53fa9a399cab
Florentin Dörre <florentin.dorre@neo4j.com>
2026-06-17 16:34:35 +0200

    Port plugin notebook tests to just target

--- src/graphdatascience/procedure_surface/cypher/catalog/__init__.py
+++ src/graphdatascience/procedure_surface/cypher/catalog/__init__.py
@@ -1,11 +1,13 @@
 from graphdatascience.procedure_surface.cypher.catalog.catalog_cypher_endpoints import (
     CatalogCypherEndpoints,
+    GraphNativeProjectEndpoints,
     GraphProjectResult,
     GraphWithProjectResult,
 )
 
 __all__ = [
     "CatalogCypherEndpoints",
+    "GraphNativeProjectEndpoints",
     "GraphProjectResult",
     "GraphWithProjectResult",
 ]
