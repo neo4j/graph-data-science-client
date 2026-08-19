4a52e52c0a9aa06dcaa78db708ed5d78bd8e96e0
Max Kießling <max.kiessling@neo4j.com>
2026-08-19 11:17:33 +0200

    Fix articulation points arrow write endpoint

--- src/graphdatascience/procedure_surface/arrow/centrality/articulationpoints_arrow_endpoints.py
+++ src/graphdatascience/procedure_surface/arrow/centrality/articulationpoints_arrow_endpoints.py
@@ -161,6 +161,9 @@ class ArticulationPointsArrowEndpoints(ArticulationPointsEndpoints):
             concurrency=concurrency,
         )
 
+        if "propertiesWritten" in result:
+            result["nodePropertiesWritten"] = result.pop("propertiesWritten")
+
         return ArticulationPointsWriteResult(**result)
 
     def estimate(
