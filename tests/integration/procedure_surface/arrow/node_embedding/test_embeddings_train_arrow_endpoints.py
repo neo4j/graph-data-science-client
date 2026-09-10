168a30572176bea80e3f281b4cb9200ea4517713
Florentin Dörre <florentin.dorre@neo4j.com>
2026-09-10 16:19:26 +0200

    Remove leaking trained model

--- tests/integration/procedure_surface/arrow/node_embedding/test_embeddings_train_arrow_endpoints.py
+++ tests/integration/procedure_surface/arrow/node_embedding/test_embeddings_train_arrow_endpoints.py
@@ -5,6 +5,7 @@ import pytest
 from graphdatascience.arrow_client.authenticated_flight_client import AuthenticatedArrowClient
 from graphdatascience.graph.graph_api import Graph
 from graphdatascience.procedure_surface.api.node_embedding.config import FastRPConfig, GBClassifierConfig
+from graphdatascience.procedure_surface.arrow.model.model_catalog_arrow_endpoints import ModelCatalogArrowEndpoints
 from graphdatascience.procedure_surface.arrow.node_embedding.train_arrow_endpoints import TrainArrowEndpoints
 from tests.integration.procedure_surface.arrow.graph_creation_helper import (
     create_graph,
@@ -32,24 +33,19 @@ def sample_graph(arrow_client_runtime: AuthenticatedArrowClient) -> Generator[Gr
         yield G
 
 
-@pytest.fixture
-def train_endpoints(
-    arrow_client_runtime: AuthenticatedArrowClient,
-) -> Generator[TrainArrowEndpoints, None, None]:
-    yield TrainArrowEndpoints(arrow_client_runtime)
-
-
 @ignore_preview_warning
-def test_embedding_train(train_endpoints: TrainArrowEndpoints, sample_graph: Graph) -> None:
-    """Test FastPath stream operation."""
-    result = train_endpoints(
-        G=sample_graph,
-        feature_properties=["x"],
-        graph_encoder=FastRPConfig(),
-        decoder=GBClassifierConfig(),
-        target_label="A",
-        target_property="y",
-        model_save_name="my_model456",
-    )
-
-    assert result.compute_millis > 0
+def test_embedding_train(arrow_client_runtime: AuthenticatedArrowClient, sample_graph: Graph) -> None:
+    model_name = "my_model456"
+    try:
+        result = TrainArrowEndpoints(arrow_client_runtime)(
+            G=sample_graph,
+            feature_properties=["x"],
+            graph_encoder=FastRPConfig(),
+            decoder=GBClassifierConfig(),
+            target_label="A",
+            target_property="y",
+            model_save_name=model_name,
+        )
+        assert result.compute_millis > 0
+    finally:
+        ModelCatalogArrowEndpoints(arrow_client_runtime).delete(model_name=model_name, fail_if_missing=True)
